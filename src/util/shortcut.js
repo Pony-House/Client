19e0334447307af729c271b56011e1a7ad05b32d
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-06-18 18:15:32 -0300

    shiftNuller

--- src/util/shortcut.js
+++ src/util/shortcut.js
@@ -1,5 +1,25 @@
+let key;
+let isShift;
+
+// Shift Hold Detector
+document.addEventListener('keydown', (ev) => {
+
+    key = ev.code;
+    isShift = ev.shiftKey; // typecast to boolean
+
+    if (isShift) {
+
+        switch (key) {
+            case 'ShiftRight': // ignore shift key
+                break;
+            default:
+                // do stuff here?
+                break;
+        }
+
+    }
+});
+
 export function shiftNuller(callback) {
-
-    callback();
-
+    if (!isShift) callback();
 }
