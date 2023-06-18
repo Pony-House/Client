b5eae530fce36bcbce045d89f3f7dcb63fdd3b30
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-06-18 18:19:27 -0300

    shortcut fix

--- src/util/shortcut.js
+++ src/util/shortcut.js
@@ -1,8 +1,6 @@
 let key;
 let isShift;
-
-// Shift Hold Detector
-document.addEventListener('keydown', (ev) => {
+const keyPressDetect = (ev) => {
 
     key = ev.code;
     isShift = ev.shiftKey; // typecast to boolean
@@ -18,7 +16,12 @@ document.addEventListener('keydown', (ev) => {
         }
 
     }
-});
+};
+
+// Shift Hold Detector
+document.addEventListener('keydown', keyPressDetect);
+document.addEventListener('keyup', keyPressDetect);
+document.addEventListener('keypress', keyPressDetect);
 
 export function shiftNuller(callback) {
     if (!isShift) callback();
