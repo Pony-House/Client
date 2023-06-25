d8b8c165d9ed44f49576355b40bd68928e2d6290
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-06-24 22:50:47 -0300

    spaceHeaderBody

--- src/app/organisms/navigation/Drawer.jsx
+++ src/app/organisms/navigation/Drawer.jsx
@@ -73,11 +73,17 @@ function Drawer() {
   if (bannerCfg && typeof bannerCfg?.url === 'string' && bannerCfg?.url.length > 0) {
     avatarSrc = mx.mxcUrlToHttp(bannerCfg.url, 960, 540);
   } else {
+
+    const spaceHeaderBody = document.querySelector('.space-drawer-body');
     const spaceHeader = document.querySelector('#space-header > .navbar');
+
+    if (spaceHeaderBody) spaceHeaderBody.classList.remove('drawer-with-banner');
+
     if (spaceHeader) {
       spaceHeader.classList.remove('banner-mode');
       spaceHeader.style.backgroundImage = '';
     }
+
   }
 
   return (
