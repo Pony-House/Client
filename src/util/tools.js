5316d2853f2bcfb59125e0029ac7bfc7b1e43b17
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-06-29 12:19:37 -0300

    resize-place - style fix

--- src/util/tools.js
+++ src/util/tools.js
@@ -1,16 +1,45 @@
+let resizePlace = null;
+let head;
+
+// Check DOM Visible
 export function checkVisible(elm) {
     const rect = elm.getBoundingClientRect();
     const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
     return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
 }
 
+// Check Resize css Fixer
 export function resizeWindowChecker(timeout = 500) {
     setTimeout(() => {
+
+        // Insert Style into head
+        if (!head) head = document.head || document.getElementsByTagName('head')[0];
+        if (head) {
+
+            // Exist DOM
+            const resizePlaceDOM = head.querySelector('#resize-place');
+
+            // Create One
+            if (!resizePlaceDOM) {
+                resizePlace = document.createElement('style');
+                resizePlace.id = 'resize-place';
+                head.appendChild(resizePlace);
+            }
+
+            // Update Value
+            else {
+                resizePlace = resizePlaceDOM;
+            }
+
+        } else { setTimeout(() => resizeWindowChecker(), 1); }
+
+        // Room View
         const roomView = document.querySelector('.room-view');
         if (roomView) {
 
             console.log(roomView.offsetWidth);
 
         }
+
     }, timeout);
 }
