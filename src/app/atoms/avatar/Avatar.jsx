41f4ac0ea0f89cd582e9f84eb95cf0a3153c38f0
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-07-02 20:39:49 -0300

    Avatar fix again

--- src/app/atoms/avatar/Avatar.jsx
+++ src/app/atoms/avatar/Avatar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import $ from 'jquery';
--- src/app/atoms/avatar/load.js
+++ src/app/atoms/avatar/load.js
@@ -7,56 +7,61 @@ export default function loadAvatar(e) {
 
     // Prepare Data
     const img = $(e.target);
-    let animParentsCount = Number(img.attr('animparentscount'));
-    if (Number.isNaN(animParentsCount) || !Number.isFinite(animParentsCount) || animParentsCount < 0 || animParentsCount > 20) animParentsCount = 0;
 
     const avatars = {
+        parents: Number(img.attr('animparentscount')),
         animate: img.attr('animsrc'),
         normal: img.attr('normalsrc'),
     };
 
+    if (Number.isNaN(avatars.parents) || !Number.isFinite(avatars.parents) || avatars.parents < 0 || avatars.parents > 20) avatars.parents = 0;
+
     // Load Data
-    getFileContentType(e, avatars.animate).then(data => {
+    img.on('load', () => {
+        getFileContentType(e, avatars.animate).then(data => {
 
-        // Set background e prepare data validator
-        img.css('background-color', 'transparent');
-        if (Array.isArray(data.type) && typeof data.type[0] === 'string' && typeof data.type[1] === 'string') {
-            if (data.type[0] === 'image') {
+            // Set background e prepare data validator
+            img.css('background-color', 'transparent');
+            if (Array.isArray(data.type) && typeof data.type[0] === 'string' && typeof data.type[1] === 'string') {
+                if (data.type[0] === 'image') {
 
-                // Gif Detected
-                img.attr('image-type', data.type[1]);
-                if (data.type[1] === 'gif') {
+                    // Gif Detected
+                    img.attr('image-type', data.type[1]);
+                    if (data.type[1] === 'gif') {
 
-                    // Prepare Node Detector
-                    let tinyNode = e.target;
-                    for (let i = 0; i < animParentsCount; i++) {
-                        tinyNode = tinyNode.parentNode;
+                        // Prepare Node Detector
+                        let tinyNode = e.target;
+                        for (let i = 0; i < avatars.parents; i++) {
+                            tinyNode = tinyNode.parentNode;
+                        }
+
+                        // Final Node
+                        tinyNode = $(tinyNode);
+
+                        // Insert Effects
+                        tinyNode.hover(
+                            () => {
+                                img.attr('src', avatars.animate);
+                            }, () => {
+                                img.attr('src', avatars.normal);
+                            }
+                        );
+
                     }
 
-                    // Final Node
-                    tinyNode = $(tinyNode);
+                    // Set Normal Image
+                    img.attr('src', avatars.normal);
 
-                    // Insert Effects
-                    tinyNode.hover(
-                        () => {
-                            img.attr('src', avatars.animate);
-                        }, () => {
-                            img.attr('src', avatars.normal);
-                        }
-                    );
-
-                }
-
-                // Set Normal Image
-                img.attr('src', avatars.normal);
-
-                // Invalid values here
+                    // Invalid values here
+                } else { img.attr('src', ImageBrokenSVG); }
             } else { img.attr('src', ImageBrokenSVG); }
-        } else { img.attr('src', ImageBrokenSVG); }
 
-    }).catch(err => {
-        console.error(err);
-        img.attr('src', ImageBrokenSVG);
+        }).catch(err => {
+            console.error(err);
+            img.attr('src', ImageBrokenSVG);
+        });
     });
+
+    img.trigger('load');
 
 };
