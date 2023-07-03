0637f7f6015904cd02f02b7929bc1878f4b5bb3f
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-07-02 23:37:45 -0300

    load avatar module - updated to promise

--- src/app/atoms/avatar/load.js
+++ src/app/atoms/avatar/load.js
@@ -5,138 +5,149 @@ import ImageBrokenSVG from '../../../../public/res/svg/image-broken.svg';
 
 // Update Avatar Data
 export function updateAvatarData(img, normalImg, animateImg) {
+    return new Promise((resolve, reject) => {
 
-    // Update Data
-    img.data('avatars-animate', animateImg);
-    img.data('avatars-normal', normalImg);
+        // Update Data
+        img.data('avatars-animate', animateImg);
+        img.data('avatars-normal', normalImg);
 
-    // Update Src
-    if (animateImg !== null) img.attr('src', animateImg);
-    else if (normalImg !== null) img.attr('src', normalImg);
+        // Update Src
+        if (animateImg !== null) img.attr('src', animateImg);
+        else if (normalImg !== null) img.attr('src', normalImg);
 
-    // Get Data
-    img.attr('loadingimg', 'true');
-    getFileContentType({ target: img.get(0) }, img.data('avatars-animate')).then(data => {
-
-        // Set Data Prepare
+        // Get Data
         img.attr('loadingimg', 'true');
+        getFileContentType({ target: img.get(0) }, img.data('avatars-animate')).then(data => {
 
-        // Read File Type
-        if (Array.isArray(data.type) && typeof data.type[0] === 'string' && typeof data.type[1] === 'string') {
-            if (data.type[0] === 'image') {
+            // Set Data Prepare
+            img.attr('loadingimg', 'true');
 
-                // Change Avatar Type
-                img.data('avatars-type', data.type[1]);
+            // Read File Type
+            if (Array.isArray(data.type) && typeof data.type[0] === 'string' && typeof data.type[1] === 'string') {
+                if (data.type[0] === 'image') {
 
-                // Set Normal Image
-                img.attr('src', img.data('avatars-normal'));
+                    // Change Avatar Type
+                    img.data('avatars-type', data.type[1]);
+
+                    // Set Normal Image
+                    img.attr('src', img.data('avatars-normal'));
+                    img.attr('loadingimg', 'false');
+
+                    // Complete
+                    resolve(true);
+
+                    // Invalid values here
+                } else { img.attr('src', ImageBrokenSVG); img.attr('loadingimg', 'false'); resolve(false); }
+            } else { img.attr('src', ImageBrokenSVG); img.attr('loadingimg', 'false'); resolve(false); }
+
+        })
+
+            // Fail
+            .catch(err => {
+                img.attr('src', ImageBrokenSVG);
                 img.attr('loadingimg', 'false');
+                reject(err);
+            });
 
-                // Invalid values here
-            } else { img.attr('src', ImageBrokenSVG); img.attr('loadingimg', 'false'); }
-        } else { img.attr('src', ImageBrokenSVG); img.attr('loadingimg', 'false'); }
-
-    })
-
-        // Fail
-        .catch(err => {
-            console.error(err);
-            img.attr('src', ImageBrokenSVG);
-            img.attr('loadingimg', 'false');
-        });
-
+    });
 };
 
 // Install Avatar Data
 export function installAvatarData(img) {
+    return new Promise((resolve, reject) => {
 
-    // Load Type Data
-    img.attr('loadingimg', 'true');
-    getFileContentType({ target: img.get(0) }, img.data('avatars-animate')).then(data => {
-
-        // Set background e prepare data validator
+        // Load Type Data
         img.attr('loadingimg', 'true');
-        img.css('background-color', 'transparent');
+        getFileContentType({ target: img.get(0) }, img.data('avatars-animate')).then(data => {
 
-        // Read File Type
-        if (Array.isArray(data.type) && typeof data.type[0] === 'string' && typeof data.type[1] === 'string') {
-            if (data.type[0] === 'image') {
+            // Set background e prepare data validator
+            img.attr('loadingimg', 'true');
+            img.css('background-color', 'transparent');
 
-                // Gif Detected
-                img.data('avatars-type', data.type[1]);
+            // Read File Type
+            if (Array.isArray(data.type) && typeof data.type[0] === 'string' && typeof data.type[1] === 'string') {
+                if (data.type[0] === 'image') {
 
-                // Prepare Node Detector
-                let tinyNode = img.get(0);
-                for (let i = 0; i < img.data('avatars-parents'); i++) {
-                    tinyNode = tinyNode.parentNode;
-                }
+                    // Gif Detected
+                    img.data('avatars-type', data.type[1]);
 
-                // Final Node
-                tinyNode = $(tinyNode);
-
-                // Insert Effects
-                tinyNode.hover(
-                    () => {
-                        if (data.type[1] === 'gif' && img.data('avatars-animate')) img.attr('src', img.data('avatars-animate'));
-                    }, () => {
-                        if (data.type[1] === 'gif' && img.data('avatars-normal')) img.attr('src', img.data('avatars-normal'));
+                    // Prepare Node Detector
+                    let tinyNode = img.get(0);
+                    for (let i = 0; i < img.data('avatars-parents'); i++) {
+                        tinyNode = tinyNode.parentNode;
                     }
-                );
 
-                // Set Normal Image
-                img.attr('src', img.data('avatars-normal'));
+                    // Final Node
+                    tinyNode = $(tinyNode);
+
+                    // Insert Effects
+                    tinyNode.hover(
+                        () => {
+                            if (data.type[1] === 'gif' && img.data('avatars-animate')) img.attr('src', img.data('avatars-animate'));
+                        }, () => {
+                            if (data.type[1] === 'gif' && img.data('avatars-normal')) img.attr('src', img.data('avatars-normal'));
+                        }
+                    );
+
+                    // Set Normal Image
+                    img.attr('src', img.data('avatars-normal'));
+                    img.attr('loadingimg', 'false');
+
+                    // Complete
+                    resolve(true);
+
+                    // Invalid values here
+                } else { img.attr('src', ImageBrokenSVG); img.attr('loadingimg', 'false'); resolve(false); }
+            } else { img.attr('src', ImageBrokenSVG); img.attr('loadingimg', 'false'); resolve(false); }
+
+        })
+
+            // Fail
+            .catch(err => {
+                img.attr('src', ImageBrokenSVG);
                 img.attr('loadingimg', 'false');
+                reject(err);
+            });
 
-                // Invalid values here
-            } else { img.attr('src', ImageBrokenSVG); img.attr('loadingimg', 'false'); }
-        } else { img.attr('src', ImageBrokenSVG); img.attr('loadingimg', 'false'); }
-
-    })
-
-        // Fail
-        .catch(err => {
-            console.error(err);
-            img.attr('src', ImageBrokenSVG);
-            img.attr('loadingimg', 'false');
-        });
-
+    });
 };
 
 // Load Avatar tags
 export function loadAvatarTags(e) {
+    return new Promise((resolve, reject) => {
 
-    // Prepare Data
-    const img = $(e.target);
-    img.attr('loadingimg', 'true');
+        // Prepare Data
+        const img = $(e.target);
+        img.attr('loadingimg', 'true');
 
-    // Get Params
-    const avatars = {
-        parents: Number(img.attr('animparentscount')),
-        animate: img.attr('animsrc'),
-        normal: img.attr('normalsrc'),
-    };
+        // Get Params
+        const avatars = {
+            parents: Number(img.attr('animparentscount')),
+            animate: img.attr('animsrc'),
+            normal: img.attr('normalsrc'),
+        };
 
-    // Insert Params
-    img.data('avatars-animate', avatars.animate);
-    img.data('avatars-normal', avatars.normal);
-    img.data('avatars-default', img.attr('defaultavatar'));
+        // Insert Params
+        img.data('avatars-animate', avatars.animate);
+        img.data('avatars-normal', avatars.normal);
+        img.data('avatars-default', img.attr('defaultavatar'));
 
-    // Delete Tags
-    img.removeAttr('animsrc');
-    img.removeAttr('animparentscount');
-    img.removeAttr('normalsrc');
-    img.removeAttr('defaultavatar');
+        // Delete Tags
+        img.removeAttr('animsrc');
+        img.removeAttr('animparentscount');
+        img.removeAttr('normalsrc');
+        img.removeAttr('defaultavatar');
 
-    // Fix Parents Param
-    if (Number.isNaN(avatars.parents) || !Number.isFinite(avatars.parents) || avatars.parents < 0 || avatars.parents > 20) avatars.parents = 0;
-    img.data('avatars-parents', avatars.parents);
+        // Fix Parents Param
+        if (Number.isNaN(avatars.parents) || !Number.isFinite(avatars.parents) || avatars.parents < 0 || avatars.parents > 20) avatars.parents = 0;
+        img.data('avatars-parents', avatars.parents);
 
-    // Update Src
-    if (avatars.animate !== null) img.attr('src', avatars.animate);
-    else if (avatars.normal !== null) img.attr('src', avatars.normal);
+        // Update Src
+        if (avatars.animate !== null) img.attr('src', avatars.animate);
+        else if (avatars.normal !== null) img.attr('src', avatars.normal);
 
-    // Load Data
-    installAvatarData(img);
-    return img;
+        // Load Data
+        installAvatarData(img).then(() => resolve(img)).catch(reject);
 
+    });
 };
