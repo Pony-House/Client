854c0c385c7c6b0fa19cd271211551a6e8e88ed5
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-07-07 13:01:00 -0300

    new builder to the yarn command

---
+++ scripts/build.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import fse from 'fs-extra';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Insert utils
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Get Path
+const srcDir = path.join(__dirname, '../repositories/twemoji/assets');
+const destDir = path.join(__dirname, '../public/img/twemoji');
+
+// To copy a folder or file, select overwrite accordingly
+try {
+    console.log('[twemoji] Installing assets files into the public folder...');
+    fse.copySync(srcDir, destDir, { overwrite: true });
+    console.log('[twemoji] Success!');
+} catch (err) {
+    console.error(err);
+}
