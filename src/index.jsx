64b8f692c28c930ae6b361b01da163d08eadfdff
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-07-12 17:52:50 -0300

    themes files

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,9 @@
+import './scss/theme/white/base.scss';
+import './scss/theme/silver/base.scss';
+import './scss/theme/dark/base.scss';
+import './scss/theme/butter/base.scss';
 import './scss/default.scss';
+
 import startApp from "./start";
 
 startApp('vite');
