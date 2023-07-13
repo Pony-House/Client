c50029e5a6d54c6be7d1ba841160d851e7dda3ec
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-07-13 01:35:39 -0300

    audio rec file - fixing some issues about first rec

--- src/util/audioRec.js
+++ src/util/audioRec.js
@@ -65,39 +65,57 @@ const audioRecorder = {
      * @returns {Promise} - returns a promise that resolves to the audio as a blob file
      */
     stop: () => new Promise(resolve => {
-        // save audio type to pass to set the Blob type
-        const mimeType = tinyCache.mediaRecorder.mimeType;
+        if (tinyCache.mediaRecorder) {
+            // save audio type to pass to set the Blob type
+            const mimeType = tinyCache.mediaRecorder.mimeType;
 
-        // listen to the stop event in order to create & return a single Blob object
-        tinyCache.mediaRecorder.addEventListener('stop', () => {
-            // create a single blob object, as we might have gathered a few Blob objects that needs to be joined as one
-            const audioBlob = new Blob(tinyCache.audioBlobs, { type: mimeType });
+            // listen to the stop event in order to create & return a single Blob object
+            tinyCache.mediaRecorder.addEventListener('stop', () => {
+                // create a single blob object, as we might have gathered a few Blob objects that needs to be joined as one
+                const audioBlob = new Blob(tinyCache.audioBlobs, { type: mimeType });
 
-            // resolve promise with the single audio blob representing the recorded audio
-            resolve(audioBlob);
-        });
-        audioRecorder.cancel();
+                // resolve promise with the single audio blob representing the recorded audio
+                resolve(audioBlob);
+            });
+            audioRecorder.cancel();
+        } else {
+            resolve(null);
+        }
     }),
 
     /** Cancel audio recording */
     cancel: () => {
-        // stop the recording feature
-        tinyCache.mediaRecorder.stop();
+        if (tinyCache.mediaRecorder) {
 
-        // stop all the tracks on the active stream in order to stop the stream
-        audioRecorder.stopStream();
+            // stop the recording feature
+            tinyCache.mediaRecorder.stop();
 
-        // reset API properties for next recording
-        audioRecorder.resetRecordingProperties();
+            // stop all the tracks on the active stream in order to stop the stream
+            audioRecorder.stopStream();
+
+            // reset API properties for next recording
+            audioRecorder.resetRecordingProperties();
+
+            return true;
+
+        }
+        return false;
     },
 
     /** Stop all the tracks on the active stream in order to stop the stream and remove
      * the red flashing dot showing in the tab
      */
     stopStream: () => {
-        // stopping the capturing request by stopping all the tracks on the active stream
-        tinyCache.streamBeingCaptured.getTracks() // get all tracks from the stream
-            .forEach(track /* of type MediaStreamTrack */ => track.stop()); // stop each one
+        if (tinyCache.streamBeingCaptured) {
+
+            // stopping the capturing request by stopping all the tracks on the active stream
+            tinyCache.streamBeingCaptured.getTracks() // get all tracks from the stream
+                .forEach(track /* of type MediaStreamTrack */ => track.stop()); // stop each one
+
+            return true;
+
+        }
+        return false;
     },
 
     /** Reset all the recording properties including the media recorder and stream being captured */
