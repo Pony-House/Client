0bc57d52eea9ecd90e3f301fcd15dc888b8ebc36
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-07-19 23:31:12 -0300

    selectedRoom.js - preparing new functions

--- src/util/selectedRoom.js
+++ src/util/selectedRoom.js
@@ -1,3 +1,53 @@
+import initMatrix from '../client/initMatrix';
+
+const eventType = 'io.pony.house.';
+
+function getDataFolderRaw(dataFolder) {
+    return initMatrix.matrixClient.getAccountData(eventType + dataFolder)?.getContent() ?? {};
+}
+
+export function getDataList(where, dataFolder) {
+    return getDataFolderRaw(dataFolder)[where];
+}
+
+export function addToDataFolder(spaceData, where, dataFolder) {
+
+    const tinyData = getDataFolderRaw(dataFolder);
+    const i = tinyData[where].findIndex(([u]) => u && u.id === spaceData.id);
+
+    let entry;
+
+    if (i < 0) {
+        entry = [spaceData, 1];
+    } else {
+        [entry] = tinyData[where].splice(i, 1);
+        entry[1] += 1;
+    }
+    tinyData[where].unshift(entry);
+
+    tinyData[where] = tinyData[where].slice(0, 100);
+    initMatrix.matrixClient.setAccountData(eventType + dataFolder, tinyData);
+
+}
+
+export function removeDataFolder(spaceData, where, dataFolder) {
+
+    const tinyData = getDataFolderRaw(dataFolder);
+
+    let index = 0;
+    while (index > -1) {
+        index = tinyData[where].findIndex(([u]) => u && u.id === spaceData.id);
+        if (index > -1) {
+            tinyData[where].splice(index, 1);
+        }
+    }
+
+    initMatrix.matrixClient.setAccountData(eventType + dataFolder, tinyData);
+
+}
+
+// getDataList('hc', 'space_temp_data');
+
 let room;
 export function setSelectRoom(body) {
     room = body;
