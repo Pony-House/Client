2e162899a5723d8d1984b88e8bdc9e04fd8144d1
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-07-29 19:24:28 -0300

    getUrlPreview - http to https

--- src/util/libs/getUrlPreview.js
+++ src/util/libs/getUrlPreview.js
@@ -13,41 +13,38 @@ setInterval(() => {
     }
 }, 60000);
 
+const urlConvert = {
+    http: (url) => `https${url.substring(4, url.length)}`,
+};
+
 export default function getUrlPreview(url, ts = 0) {
     return new Promise((resolve, reject) => {
         const mx = initMatrix.matrixClient;
         if (typeof url === 'string' && linkify.test(url)) {
+
             if (
-
                 tinyCache[url] &&
-
-                (
-                    objType(tinyCache[url].data, 'object') ||
-                    tinyCache[url].data === null
-                )
-
+                (objType(tinyCache[url].data, 'object') || tinyCache[url].data === null)
             ) {
                 resolve(tinyCache[url].data);
             } else {
-                mx.getUrlPreview(url, ts).then(embed => {
 
-                    tinyCache[url] = {
-                        data: embed,
-                        timeout: 60
-                    };
+                let tinyUrl = url;
+                for (const item in urlConvert) {
+                    if (tinyUrl.startsWith(`${item}://`)) {
+                        tinyUrl = urlConvert[item](tinyUrl);
+                        break;
+                    }
+                }
 
+                mx.getUrlPreview(tinyUrl, ts).then(embed => {
+                    tinyCache[url] = { data: embed, timeout: 60 };
                     resolve(embed);
-
                 }).catch(err => {
-
-                    tinyCache[url] = {
-                        data: null,
-                        timeout: 60
-                    };
-
+                    tinyCache[url] = { data: null, timeout: 60 };
                     reject(err);
-
                 });
+
             }
 
         }
