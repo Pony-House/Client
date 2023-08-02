011fa4bec465b57f958a56d19824e65fa00ee85e
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-08-02 19:23:29 -0300

    keywords - base prepare

--- mods/keywords.js
+++ mods/keywords.js
@@ -1,340 +1,344 @@
+const defaultNames = {
+
+};
+
 export default [
 
     // Booru
     {
-        title: 'Derpibooru - Philomena Booru',
-        name: 'derpibooru',
-        href: 'https://derpibooru.org/'
+        title: `Derpibooru - Philomena Booru`,
+        name: `derpibooru`,
+        href: `https://derpibooru.org/`
     },
 
     {
-        title: 'Furbooru - Philomena Booru',
-        name: 'furbooru',
-        href: 'https://furbooru.org/'
+        title: `Furbooru - Philomena Booru`,
+        name: `furbooru`,
+        href: `https://furbooru.org/`
     },
 
     // Dev
     {
-        title: 'Github',
-        name: 'github',
-        href: 'https://github.com/'
+        title: `Github`,
+        name: `github`,
+        href: `https://github.com/`
     },
 
     {
-        title: 'Gitlab',
-        name: 'gitlab',
-        href: 'https://gitlab.com/'
+        title: `Gitlab`,
+        name: `gitlab`,
+        href: `https://gitlab.com/`
     },
 
     {
-        title: 'Ubuntu - Linux OS',
-        name: 'ubuntu',
-        href: 'https://ubuntu.com/'
+        title: `Ubuntu - Linux OS`,
+        name: `ubuntu`,
+        href: `https://ubuntu.com/`
     },
 
     {
-        title: 'IPFS - Inter Planetary File System',
-        name: 'ipfs',
-        href: 'https://ipfs.tech/'
+        title: `IPFS - Inter Planetary File System`,
+        name: `ipfs`,
+        href: `https://ipfs.tech/`
     },
 
     // Games
     {
-        title: 'Steam Games',
-        name: ['steam', 'valve'],
-        href: 'https://store.steampowered.com/'
+        title: `Steam Games`,
+        name: [`steam`, `valve`],
+        href: `https://store.steampowered.com/`
     },
 
     {
-        title: 'GOG Games',
-        name: 'gog',
-        href: 'https://www.gog.com/'
+        title: `GOG Games`,
+        name: `gog`,
+        href: `https://www.gog.com/`
     },
 
     {
-        title: 'Electronic Arts',
-        name: ['eletronic arts', 'ea games', 'ea'],
-        href: 'https://www.ea.com/'
+        title: `Electronic Arts`,
+        name: [`eletronic arts`, `ea games`, `ea`],
+        href: `https://www.ea.com/`
     },
 
     // Art
     {
-        title: 'Fur Affinity',
-        name: 'furaffinity',
-        href: 'https://www.furaffinity.net/'
+        title: `Fur Affinity`,
+        name: `furaffinity`,
+        href: `https://www.furaffinity.net/`
     },
 
     {
-        title: 'DeviantArt',
-        name: 'deviantart',
-        href: 'https://www.deviantart.com/'
+        title: `DeviantArt`,
+        name: `deviantart`,
+        href: `https://www.deviantart.com/`
     },
 
     // Tiny me
     {
-        title: 'Tiny Jasmini - Pony House Founder',
-        name: ['jasmindreasond', 'tiny jasmini', 'jasmini'],
-        href: 'https://www.google.com/search?q=jasmindreasond'
+        title: `Tiny Jasmini - Pony House Founder`,
+        name: [`jasmindreasond`, `tiny jasmini`, `jasmini`],
+        href: `https://www.google.com/search?q=jasmindreasond`
     },
 
     {
-        title: 'Puddy Club - JasminDreasond Website',
-        name: 'puddy club',
-        href: 'https://puddy.club/'
+        title: `Puddy Club - JasminDreasond Website`,
+        name: `puddy club`,
+        href: `https://puddy.club/`
     },
 
     // Social
     {
-        title: 'Google',
-        name: 'google',
-        href: 'https://google.com/'
+        title: `Google`,
+        name: `google`,
+        href: `https://google.com/`
     },
 
     {
-        title: 'Patreon',
-        name: 'patreon',
-        href: 'https://www.patreon.com/'
+        title: `Patreon`,
+        name: `patreon`,
+        href: `https://www.patreon.com/`
     },
 
     {
-        title: 'Keybase',
-        name: 'keybase',
-        href: 'https://keybase.io/'
+        title: `Keybase`,
+        name: `keybase`,
+        href: `https://keybase.io/`
     },
 
     {
-        title: 'Twitch',
-        name: 'twitch',
-        href: 'https://www.twitch.tv/'
+        title: `Twitch`,
+        name: `twitch`,
+        href: `https://www.twitch.tv/`
     },
 
     {
-        title: 'SoundCloud',
-        name: 'soundcloud',
-        href: 'https://soundcloud.com/'
+        title: `SoundCloud`,
+        name: `soundcloud`,
+        href: `https://soundcloud.com/`
     },
 
     {
-        title: 'Matrix Protocol',
-        name: 'matrix',
-        href: 'https://matrix.org/'
+        title: `Matrix Protocol`,
+        name: `matrix`,
+        href: `https://matrix.org/`
     },
 
     {
-        title: 'Pony House - Matrix Client',
-        name: 'pony house',
-        href: 'https://pony.house/'
+        title: `Pony House - Matrix Client`,
+        name: `pony house`,
+        href: `https://pony.house/`
     },
 
     {
-        title: 'Discord',
-        name: 'discord',
-        href: 'https://discord.com/'
+        title: `Discord`,
+        name: `discord`,
+        href: `https://discord.com/`
     },
 
     {
-        title: 'X.com',
-        name: ['twitter', 'x.com'],
-        href: 'https://x.com/'
+        title: `X.com`,
+        name: [`twitter`, `x.com`],
+        href: `https://x.com/`
     },
 
     {
-        title: 'Youtube',
-        name: 'youtube',
-        href: 'https://youtube.com/'
+        title: `Youtube`,
+        name: `youtube`,
+        href: `https://youtube.com/`
     },
 
     {
-        title: 'Mastodon',
-        name: 'mastodon',
-        href: 'https://joinmastodon.org/'
+        title: `Mastodon`,
+        name: `mastodon`,
+        href: `https://joinmastodon.org/`
     },
 
     {
-        title: 'Equestria Social - Mastodon Social Media',
-        name: 'equestria social',
-        href: 'https://equestria.social/'
+        title: `Equestria Social - Mastodon Social Media`,
+        name: `equestria social`,
+        href: `https://equestria.social/`
     },
 
     // Crypto Social
     {
-        title: 'Lenster - Lens Web3 Application',
-        name: 'lenster',
-        href: 'https://lenster.xyz/'
+        title: `Lenster - Lens Web3 Application`,
+        name: `lenster`,
+        href: `https://lenster.xyz/`
     },
 
     {
-        title: 'Lens Protocol',
-        name: 'lens',
-        href: 'https://www.lens.xyz/'
+        title: `Lens Protocol`,
+        name: `lens`,
+        href: `https://www.lens.xyz/`
     },
 
     {
-        title: 'Odysee',
-        name: 'odysee',
-        href: 'https://odysee.com/'
+        title: `Odysee`,
+        name: `odysee`,
+        href: `https://odysee.com/`
     },
 
     // Crypto
     {
-        title: 'Binance - Crypto Exchange',
-        name: 'binance',
-        href: 'https://binance.com/'
+        title: `Binance - Crypto Exchange`,
+        name: `binance`,
+        href: `https://binance.com/`
     },
 
     {
-        title: 'Coinbase - Crypto Exchange',
-        name: 'coinbase',
-        href: 'https://coinbase.com/'
+        title: `Coinbase - Crypto Exchange`,
+        name: `coinbase`,
+        href: `https://coinbase.com/`
     },
 
     {
-        title: 'Unstoppable Domains - Web3 Registrar',
-        name: 'unstoppable domains',
-        href: 'https://unstoppabledomains.com/'
+        title: `Unstoppable Domains - Web3 Registrar`,
+        name: `unstoppable domains`,
+        href: `https://unstoppabledomains.com/`
     },
 
     {
-        title: 'CoinMarketCap',
-        name: 'crypto prices',
-        href: 'https://coinmarketcap.com/'
+        title: `CoinMarketCap`,
+        name: `crypto prices`,
+        href: `https://coinmarketcap.com/`
     },
 
     {
-        title: 'OpenSea - NFT Marketplace',
-        name: 'opensea',
-        href: 'https://opensea.io/'
+        title: `OpenSea - NFT Marketplace`,
+        name: `opensea`,
+        href: `https://opensea.io/`
     },
 
     {
-        title: 'Rarible - NFT Marketplace',
-        name: 'rarible',
-        href: 'https://rarible.com/'
+        title: `Rarible - NFT Marketplace`,
+        name: `rarible`,
+        href: `https://rarible.com/`
     },
 
     // DEX
     {
-        title: 'Uniswap Ecosystem - DeFi',
-        name: 'uniswap',
-        href: 'https://uniswap.org/'
+        title: `Uniswap Ecosystem - DeFi`,
+        name: `uniswap`,
+        href: `https://uniswap.org/`
     },
 
     {
-        title: 'QuickSwap - DeFi',
-        name: 'quickswap',
-        href: 'https://quickswap.exchange/'
+        title: `QuickSwap - DeFi`,
+        name: `quickswap`,
+        href: `https://quickswap.exchange/`
     },
 
     {
-        title: 'PancakeSwap - DeFi',
-        name: ['pancakeswap', 'pancake swap'],
-        href: 'https://pancakeswap.finance/'
+        title: `PancakeSwap - DeFi`,
+        name: [`pancakeswap`, `pancake swap`],
+        href: `https://pancakeswap.finance/`
     },
 
     // Cryptocurrency
     {
-        title: 'Polygon - Blockchain',
-        name: ['polygon', 'matic'],
-        href: 'https://polygon.technology/'
+        title: `Polygon - Blockchain`,
+        name: [`polygon`, `matic`],
+        href: `https://polygon.technology/`
     },
 
     {
-        title: 'Ethereum - Blockchain',
-        name: ['ethereum', 'eth'],
-        href: 'https://ethereum.org/'
+        title: `Ethereum - Blockchain`,
+        name: [`ethereum`, `eth`],
+        href: `https://ethereum.org/`
     },
 
     {
-        title: 'Optimism - Blockchain',
-        name: ['optimism', '$op'],
-        href: 'https://www.optimism.io/'
+        title: `Optimism - Blockchain`,
+        name: [`optimism`, `$op`],
+        href: `https://www.optimism.io/`
     },
 
     {
-        title: 'Arbitrum - Blockchain',
-        name: ['arbitrum', '$arb'],
-        href: 'https://arbitrum.io/'
+        title: `Arbitrum - Blockchain`,
+        name: [`arbitrum`, `$arb`],
+        href: `https://arbitrum.io/`
     },
 
     {
-        title: 'Arweave - Blockchain',
-        name: ['arweave', '$arb'],
-        href: 'https://arbitrum.io/'
+        title: `Arweave - Blockchain`,
+        name: [`arweave`, `$arb`],
+        href: `https://arbitrum.io/`
     },
 
     {
-        title: 'LBRY - Blockchain',
-        name: ['lbry'],
-        href: 'https://lbry.com/'
+        title: `LBRY - Blockchain`,
+        name: [`lbry`],
+        href: `https://lbry.com/`
     },
 
     {
-        title: 'Dogecoin - Cryptocurrency',
-        name: ['dogecoin', 'doge'],
-        href: 'https://dogecoin.com/'
+        title: `Dogecoin - Cryptocurrency`,
+        name: [`dogecoin`, `doge`],
+        href: `https://dogecoin.com/`
     },
 
     {
-        title: 'Bitcoin - Cryptocurrency',
-        name: ['bitcoin', 'btc'],
-        href: 'https://bitcoin.org/'
+        title: `Bitcoin - Cryptocurrency`,
+        name: [`bitcoin`, `btc`],
+        href: `https://bitcoin.org/`
     },
 
     {
-        title: 'Monero - Cryptocurrency',
-        name: ['monero', 'xmr'],
-        href: 'https://www.getmonero.org/'
+        title: `Monero - Cryptocurrency`,
+        name: [`monero`, `xmr`],
+        href: `https://www.getmonero.org/`
     },
 
     // Tokens USD
     {
-        title: 'Circle - Crypto Company',
-        name: 'circle',
-        href: 'https://www.circle.com/'
+        title: `Circle - Crypto Company`,
+        name: `circle`,
+        href: `https://www.circle.com/`
     },
 
     {
-        title: 'USD Coin - Crypto Token',
-        name: ['usdc', 'usd coin'],
-        href: 'https://www.circle.com/en/usdc'
+        title: `USD Coin - Crypto Token`,
+        name: [`usdc`, `usd coin`],
+        href: `https://www.circle.com/en/usdc`
     },
 
     {
-        title: 'Dollar Tether - Crypto Token',
-        name: ['usdt', 'tether'],
-        href: 'https://tether.to/'
+        title: `Dollar Tether - Crypto Token`,
+        name: [`usdt`, `tether`],
+        href: `https://tether.to/`
     },
 
     // Crypto Token
     {
-        title: 'Shiba - Crypto Token',
-        name: ['shiba token', '$shib'],
-        href: 'https://www.shibatoken.com/'
+        title: `Shiba - Crypto Token`,
+        name: [`shiba token`, `$shib`],
+        href: `https://www.shibatoken.com/`
     },
 
     // Crypto Wallet
     {
-        title: 'Metamask Wallet - Crypto Self-Custody',
-        name: 'metamask',
-        href: 'https://metamask.io/'
+        title: `Metamask Wallet - Crypto Self-Custody`,
+        name: `metamask`,
+        href: `https://metamask.io/`
     },
 
     {
-        title: 'Rabby Wallet - Crypto Self-Custody',
-        name: 'rabby',
-        href: 'https://rabby.io/'
+        title: `Rabby Wallet - Crypto Self-Custody`,
+        name: `rabby`,
+        href: `https://rabby.io/`
     },
 
     {
-        title: 'Frame Wallet - Crypto Self-Custody',
-        name: 'frame wallet',
-        href: 'https://frame.sh/'
+        title: `Frame Wallet - Crypto Self-Custody`,
+        name: `frame wallet`,
+        href: `https://frame.sh/`
     },
 
     {
-        title: 'Trust Wallet - Crypto Self-Custody',
-        name: 'trust wallet',
-        href: 'https://trustwallet.com/'
+        title: `Trust Wallet - Crypto Self-Custody`,
+        name: `trust wallet`,
+        href: `https://trustwallet.com/`
     },
 
 ];
