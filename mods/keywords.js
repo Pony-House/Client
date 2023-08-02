a4a274b423b45e537fb485e8bdf132be96b7c61d
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-08-02 11:50:42 -0300

    keywords

--- mods/keywords.js
+++ mods/keywords.js
@@ -1,8 +1,184 @@
 export default [
 
+    // Booru
+    {
+        name: 'derpibooru',
+        href: 'https://derpibooru.org/'
+    },
+
+    {
+        name: 'furbooru',
+        href: 'https://furbooru.org/'
+    },
+
+    // Dev
+    {
+        name: 'github',
+        href: 'https://github.com/'
+    },
+
+    {
+        name: 'gitlab',
+        href: 'https://gitlab.com/'
+    },
+
+    // Games
+    {
+        name: 'steam',
+        href: 'https://store.steampowered.com/'
+    },
+
+    {
+        name: 'gog',
+        href: 'https://www.gog.com/'
+    },
+
+    {
+        name: 'ea',
+        href: 'https://www.ea.com/'
+    },
+
+    // Art
+    {
+        name: 'furaffinity',
+        href: 'https://www.furaffinity.net/'
+    },
+
+    {
+        name: 'deviantart',
+        href: 'https://www.deviantart.com/'
+    },
+
+    // Social
+    {
+        name: 'pony house',
+        href: 'https://pony.house/'
+    },
+
+    {
+        name: 'discord',
+        href: 'https://discord.com/'
+    },
+
     {
         name: 'twitter',
-        href: 'https://x.com'
-    }
+        href: 'https://x.com/'
+    },
+
+    {
+        name: 'youtube',
+        href: 'https://youtube.com/'
+    },
+
+    // Crypto Social
+    {
+        name: 'lenster',
+        href: 'https://lenster.xyz/'
+    },
+
+    {
+        name: 'lens',
+        href: 'https://www.lens.xyz/'
+    },
+
+    {
+        name: 'odysee',
+        href: 'https://odysee.com/'
+    },
+
+    // Crypto
+    {
+        name: 'binance',
+        href: 'https://binance.com/'
+    },
+
+    {
+        name: 'coinbase',
+        href: 'https://coinbase.com/'
+    },
+
+    {
+        name: 'unstoppable domains',
+        href: 'https://unstoppabledomains.com/'
+    },
+
+    {
+        name: 'crypto prices',
+        href: 'https://coinmarketcap.com/'
+    },
+
+    // DEX
+    {
+        name: 'uniswap',
+        href: 'https://uniswap.org/'
+    },
+
+    {
+        name: 'quickswap',
+        href: 'https://quickswap.exchange/'
+    },
+
+    {
+        name: 'pancakeswap',
+        href: 'https://pancakeswap.finance/'
+    },
+
+    // Cryptocurrency
+    {
+        name: 'polygon',
+        href: 'https://polygon.technology/'
+    },
+
+    {
+        name: 'ethereum',
+        href: 'https://ethereum.org/'
+    },
+
+    {
+        name: 'optimism',
+        href: 'https://www.optimism.io/'
+    },
+
+    {
+        name: 'arbitrum',
+        href: 'https://offchainlabs.com/'
+    },
+
+    {
+        name: 'dogecoin',
+        href: 'https://dogecoin.com/'
+    },
+
+    {
+        name: 'bitcoin',
+        href: 'https://bitcoin.org/'
+    },
+
+    // Tokens USD
+    {
+        name: 'usdc',
+        href: 'https://www.circle.com/en/usdc'
+    },
+
+    {
+        name: 'usd coin',
+        href: 'https://www.circle.com/en/usdc'
+    },
+
+    {
+        name: 'usdt',
+        href: 'https://tether.to/'
+    },
+
+    {
+        name: 'tether',
+        href: 'https://tether.to/'
+    },
+
+    // Crypto Token
+    {
+        name: '$shib',
+        href: 'https://www.shibatoken.com/'
+    },
 
 ];
