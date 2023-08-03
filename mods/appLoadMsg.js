c6cad7ec6a11dfd98511f1dd9d9b19c567107559
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-08-02 23:43:59 -0300

    app load messages - tiny update list

--- mods/appLoadMsg.js
+++ mods/appLoadMsg.js
@@ -9,7 +9,34 @@ export default {
         ],
 
         items: [
-            'Heating up!'
+
+            '8469',
+            'Vires in numerics',
+            'Pika a boo!',
+            'Yay!',
+
+            'Have you ever tried typing 8469 on some old keyboard?',
+            '8469 was JasminDreasond\'s old Discord discriminator. Tiny Jasmini#8469',
+
+            'How many words are tiny on the name Tiny Jasmini?',
+
+            'Wow! Does he really have the power to control the most valuable dog on the internet?',
+            'The founder of Pony House just started this project as a response to the country\'s government trying to censor the entire country\'s internet.',
+            'Dont be silly! Do not use a letter as a name for your app.',
+            'Do you know what an X-Tudo is for brazilians?',
+
+            'Why are you reading this?',
+            'Looking for your ponies...',
+            'Looking for your puddings...',
+
+            'Oh my goodness, here we go again.',
+
+            'Where are your puddings while this is loading?',
+
+            'Before Pony House had this name, the app was called Puddy Cinny. Yes. This app was born as a Cinny fork.',
+
+            'The name of the founder of the Pony House project is JasminDreasond.',
+            'The Pony House founder will always want your puddings! Keep your puddings safe!',
         ],
 
     }
