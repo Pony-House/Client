f1a7f1eab0de1078345a4b4a88f82c1ac12837da
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-08-03 00:52:35 -0300

    mod disabled by cors error

--- mods/index.js
+++ mods/index.js
@@ -1,12 +1,12 @@
-import sinkingYachts from './sinking.yachts';
 // import startTest from './test';
 // import './test/style.scss';
 
 // import helloWorld from './hello-world';
+// import sinkingYachts from './sinking.yachts';
 
 export default function startMods() {
 
-    sinkingYachts();
+    // sinkingYachts();
     // helloWorld();
     // startTest();
 
