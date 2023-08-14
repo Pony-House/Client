cf6794f6120cfd4a827fa8c3cfe2dc3b5c9218bf
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-08-14 00:25:36 -0300

    electron mode test

--- src/client/initMatrix.js
+++ src/client/initMatrix.js
@@ -11,8 +11,27 @@ import { cryptoCallbacks } from './state/secretStorageKeys';
 import navigation from './state/navigation';
 import logger from './logger';
 
-global.Olm = Olm;
+if (__ENV_APP__.electron_mode) {
+  global.Olm = {
+    // eslint-disable-next-line object-shorthand
+    init: function () {
 
+      const args = [];
+      for (const item in arguments) {
+        args.push(arguments[item]);
+      }
+
+      if (!args[0]) args.push({});
+      args[0].locateFile = () => '/olm.wasm';
+
+      global.Olm = Olm;
+      Olm.init.apply(Olm, args);
+
+    }
+  };
+} else {
+  global.Olm = Olm;
+}
 
 class InitMatrix extends EventEmitter {
   constructor() {
