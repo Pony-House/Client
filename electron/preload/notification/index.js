6801dbd9bf3dad46b1f8d11ad07f3fffef84dc1d
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-08-15 03:08:03 -0300

    ipcRenderer - items

--- electron/preload/notification/index.js
+++ electron/preload/notification/index.js
@@ -1,5 +1,33 @@
 import { ipcRenderer } from 'electron';
 
+ipcRenderer.on('tiny-notification-create', (_event, arg) => {
+
+});
+
+ipcRenderer.on('tiny-notification-show', (_event, arg) => {
+
+});
+
+ipcRenderer.on('tiny-notification-click', (_event, arg) => {
+
+});
+
+ipcRenderer.on('tiny-notification-close', (_event, arg) => {
+
+});
+
+ipcRenderer.on('tiny-notification-reply', (_event, arg) => {
+
+});
+
+ipcRenderer.on('tiny-notification-action', (_event, arg) => {
+
+});
+
+ipcRenderer.on('tiny-notification-failed', (_event, arg) => {
+
+});
+
 // Module
 const notifications = {};
 export default function startNotifications(options) {
