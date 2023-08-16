c099a23c89b4726a020ed932113871dee18a36ed
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-08-16 20:39:32 -0300

    tray and app progress

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -38,6 +38,8 @@ const title = 'Pony House';
 let appShow: boolean;
 let isQuiting: boolean;
 let appStarted: boolean;
+let firstTime = false;
+let appReady = false;
 
 let win: BrowserWindow | null = null;
 // Here, you can also use other preload
@@ -47,118 +49,85 @@ const indexHtml = path.join(process.env.DIST, 'index.html');
 const icon = path.join(process.env.VITE_PUBLIC, './favicon.ico');
 
 async function createWindow() {
-  win = new BrowserWindow({
-    title,
-    icon,
-    show: false,
-    webPreferences: {
-      preload,
-      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
-      nodeIntegration: true,
-      contextIsolation: true,
-    },
-  });
+  if (!firstTime) {
+    firstTime = true;
 
-  startResizeEvents(ipcMain, win);
-  startEvents(ipcMain, win);
-  startNotifications(ipcMain, win);
-
-  win.removeMenu();
-
-  if (tinyUrl) {
-    // electron-vite-vue#298
-    win.loadURL(tinyUrl);
-    // Open devTool if the app is not packaged
-    win.webContents.openDevTools();
-  } else {
-    win.loadFile(indexHtml);
-  }
-
-  // Show Page
-  win.once('ready-to-show', () => {
-    if (win) win.show();
-    appStarted = true;
-    appShow = true;
-
-    // Ping
-    if (win) {
-      win.webContents.send('ping', {
-        DIST_ELECTRON: process.env.DIST_ELECTRON,
-        DIST: process.env.DIST,
-        VITE_DEV_SERVER_URL: process.env.VITE_DEV_SERVER_URL,
-        VITE_PUBLIC: process.env.VITE_PUBLIC,
-        platform: process.platform,
+    win = new BrowserWindow({
+      title,
+      icon,
+      show: false,
+      webPreferences: {
         preload,
-        tinyUrl,
-        indexHtml,
-        icon,
-        title,
-      });
-    }
-  });
+        // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+        nodeIntegration: true,
+        contextIsolation: true,
+      },
+    });
 
-  // Make all links open with the browser, not with the application
-  win.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith('https:')) shell.openExternal(url);
-    return { action: 'deny' };
-  });
+    startResizeEvents(ipcMain, win);
+    startEvents(ipcMain, win);
+    startNotifications(ipcMain, win);
 
-  // Apply electron-updater
-  update(win);
+    win.removeMenu();
 
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-
-  // Prevent Close
-  win.on('close', (event) => {
-    if (!isQuiting) {
-      event.preventDefault();
-      if (win) win.hide();
-      appShow = false;
+    if (tinyUrl) {
+      // electron-vite-vue#298
+      win.loadURL(tinyUrl);
+      // Open devTool if the app is not packaged
+      win.webContents.openDevTools();
+    } else {
+      win.loadFile(indexHtml);
     }
 
-    return false;
-  });
+    // Show Page
+    win.once('ready-to-show', () => {
+      if (win) win.show();
+      appStarted = true;
+      appShow = true;
 
-  // Create Tray
-  const tray = new Tray(icon);
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Show App',
-      click: () => {
-        if (appStarted) {
-          if (win) win.show();
-          appShow = true;
-        }
-      },
-    },
-    {
-      label: 'Quit',
-      click: () => {
-        isQuiting = true;
-        app.quit();
-      },
-    },
-  ]);
+      // Ping
+      if (win) {
+        win.webContents.send('ping', {
+          DIST_ELECTRON: process.env.DIST_ELECTRON,
+          DIST: process.env.DIST,
+          VITE_DEV_SERVER_URL: process.env.VITE_DEV_SERVER_URL,
+          VITE_PUBLIC: process.env.VITE_PUBLIC,
+          platform: process.platform,
+          preload,
+          tinyUrl,
+          indexHtml,
+          icon,
+          title,
+        });
+      }
+    });
 
-  tray.setToolTip(title);
-  tray.setTitle(title);
-  tray.setContextMenu(contextMenu);
+    // Make all links open with the browser, not with the application
+    win.webContents.setWindowOpenHandler(({ url }) => {
+      if (url.startsWith('https:')) shell.openExternal(url);
+      return { action: 'deny' };
+    });
 
-  tray.on('double-click', () => {
-    if (appStarted) {
-      if (!appShow) {
-        if (win) win.show();
-        appShow = true;
-      } else {
+    // Apply electron-updater
+    update(win);
+
+    app.on('activate', () => {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) createWindow('activate');
+    });
+
+    // Prevent Close
+    win.on('close', (event) => {
+      if (!isQuiting) {
+        event.preventDefault();
         if (win) win.hide();
         appShow = false;
       }
-    }
-  });
+
+      return false;
+    });
+  }
 }
 
 // Anti Multi Same App
@@ -180,7 +149,48 @@ if (!gotTheLock) {
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
-  app.whenReady().then(createWindow);
+  app.whenReady().then(() => createWindow('ready'));
+  app.on('ready', () => {
+    if (!appReady) {
+      appReady = true;
+      // Create Tray
+      const tray = new Tray(icon);
+      const contextMenu = Menu.buildFromTemplate([
+        {
+          label: 'Show App',
+          click: () => {
+            if (appStarted) {
+              if (win) win.show();
+              appShow = true;
+            }
+          },
+        },
+        {
+          label: 'Quit',
+          click: () => {
+            isQuiting = true;
+            app.quit();
+          },
+        },
+      ]);
+
+      tray.setToolTip(title);
+      tray.setTitle(title);
+      tray.setContextMenu(contextMenu);
+
+      tray.on('double-click', () => {
+        if (appStarted) {
+          if (!appShow) {
+            if (win) win.show();
+            appShow = true;
+          } else {
+            if (win) win.hide();
+            appShow = false;
+          }
+        }
+      });
+    }
+  });
 }
 
 app.on('window-all-closed', () => {
@@ -202,44 +212,7 @@ app.on('activate', () => {
   if (allWindows.length) {
     allWindows[0].focus();
   } else {
-    createWindow();
-  }
-});
-
-// Create Tray
-const tray = new Tray(icon);
-const contextMenu = Menu.buildFromTemplate([
-  {
-    label: 'Show App',
-    click: () => {
-      if (appStarted) {
-        if (win) win.show();
-        appShow = true;
-      }
-    },
-  },
-  {
-    label: 'Quit',
-    click: () => {
-      isQuiting = true;
-      app.quit();
-    },
-  },
-]);
-
-tray.setToolTip(title);
-tray.setTitle(title);
-tray.setContextMenu(contextMenu);
-
-tray.on('double-click', () => {
-  if (appStarted) {
-    if (!appShow) {
-      if (win) win.show();
-      appShow = true;
-    } else {
-      if (win) win.hide();
-      appShow = false;
-    }
+    createWindow('activate-2');
   }
 });
 
