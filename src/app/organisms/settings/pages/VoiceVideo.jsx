fc53f4cbfa2b784a77dd034a54ff3c7ca5afe0ac
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-08-18 03:02:09 -0300

    Voice & Video - template ready

--- src/app/organisms/settings/pages/VoiceVideo.jsx
+++ src/app/organisms/settings/pages/VoiceVideo.jsx
@@ -29,10 +29,12 @@ const listDevices = async () => {
 
 };
 
-
 function VoiceVideoSection() {
 
     const [devicesItem, setDevicesItem] = useState(null);
+    const [audioDevice, setAudioDevice] = useState(global.localStorage.getItem('tinyAudioDevice'));
+    const [videoDevice, setVideoDevice] = useState(global.localStorage.getItem('tinyVideoDevice'));
+
     useEffect(() => {
 
         if (!loadingDevices && devicesItem === null) {
@@ -52,10 +54,100 @@ function VoiceVideoSection() {
 
     console.log(devicesItem);
 
-    return (
-        <div className="noselect">
+    return (<>
 
+        <div className="card noselect">
+            <ul className="list-group list-group-flush">
+
+                <li className="list-group-item very-small text-gray">Voice Settings</li>
+
+                <li className="list-group-item border-0">
+
+                    <div className='row'>
+
+                        <div className='col-md-6'>
+                            <div className='very-small text-uppercase fw-bold mb-2'>Input Device</div>
+                            <select class="form-select form-control-bg">
+                                <option selected>Choose...</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+
+                        <div className='col-md-6'>
+                            <div className='very-small text-uppercase fw-bold mb-2'>Output Device</div>
+                            <select class="form-select form-control-bg">
+                                <option selected>Choose...</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+
+                    </div>
+
+                </li>
+
+                <li className="list-group-item border-0">
+
+                    <div className='row'>
+
+                        <div className='col-md-6'>
+                            <div className='very-small text-uppercase fw-bold mb-2'>Input Volume</div>
+                            <input type="range" class="form-range" />
+                        </div>
+
+                        <div className='col-md-6'>
+                            <div className='very-small text-uppercase fw-bold mb-2'>Output Volume</div>
+                            <input type="range" class="form-range" />
+                        </div>
+
+                    </div>
+
+                </li>
+
+                <li className="list-group-item border-0">
+                    <div className='very-small text-uppercase fw-bold mb-1'>Mic Test</div>
+                    <div className='very-small mb-2'>Having mic issues? Start a test and say something fun. Your voice will be played back to you.</div>
+                    <div class="input-group input-group-sm mb-3">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon1">Let&apos;s Check</button>
+                        <input type="text" class="form-control form-control-bg" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" />
+                    </div>
+                </li>
+
+
+
+            </ul>
         </div>
+
+        <div className="card noselect mt-3">
+            <ul className="list-group list-group-flush">
+
+                <li className="list-group-item very-small text-gray">Video Settings</li>
+
+                <li className="list-group-item border-0">
+
+                    <div class="ratio ratio-16x9 w-50 border border-bg mb-2">
+
+
+
+                    </div>
+
+                    <div className='very-small text-uppercase fw-bold mb-2'>Camera</div>
+                    <select class="form-select form-control-bg">
+                        <option selected>Choose...</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+
+                </li>
+
+            </ul>
+        </div>
+
+    </>
     );
 
 };
