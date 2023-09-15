c82d1f364bf03d125f7f3742f55d9c8ee006fba5
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-09-14 21:42:42 -0300

    ethereum checker fixed

--- src/util/web3/status.js
+++ src/util/web3/status.js
@@ -1,6 +1,6 @@
 import { toast } from "../tools";
 
-let header = {};
+let header;
 
 export default function startStatus() {
 
@@ -14,29 +14,29 @@ export default function startStatus() {
 
             // Update Data
             if (!global.tinyCrypto.matrixEthereum) {
-                global.tinyCrypto.matrixEthereum = $('meta[name="user-ethereum-address"]').attr('content').toLowerCase();
+                global.tinyCrypto.matrixEthereum = $('meta[name="user-ethereum-address"]').attr('content')?.toLowerCase();
             }
 
             // Check Data and Insert Warn
             if (global.tinyCrypto.matrixEthereum !== global.tinyCrypto.address) {
 
-                header?.css('opacity', 0.7);
+                if (header) header.css('opacity', 0.7);
 
                 if (global.tinyCrypto.accounts.length > 0) {
 
-                    header?.css('color', 'red');
-                    header?.attr('title', 'Your ethereum wallet is not the same as your Derpibooru account.');
-                    toast('Your ethereum wallet is not the same as your Derpibooru account!', 'Ethereum Wallet');
+                    if (header) header.css('color', 'red');
+                    if (header) header.attr('title', 'Your ethereum wallet is not the same as your matrix account.');
+                    toast('Your ethereum wallet is not the same as your matrix account!', 'Ethereum Wallet');
 
-                    // connectWallet.html(`${ethIcon} Your wallet does not share the same value as your Derpibooru account. You can click here to try to reconnect a new address.`);
+                    // connectWallet.html(`${ethIcon} Your wallet does not share the same value as your matrix account. You can click here to try to reconnect a new address.`);
 
                 }
 
                 else {
 
                     const tinyMessage = 'Please reconnect your crypto wallet! A new window will open to redo this.';
-                    header?.css('color', 'yellow');
-                    header?.attr('title', tinyMessage);
+                    if (header) header.css('color', 'yellow');
+                    if (header) header.attr('title', tinyMessage);
                     toast(tinyMessage, 'Ethereum Wallet');
 
                     // connectWallet.eth(`${ethIcon} ${tinyMessage}`);
@@ -55,14 +55,14 @@ export default function startStatus() {
         else {
             toast('You don\'t have a ethereum Wallet installed in your browser!', 'Ethereum Wallet');
             global.tinyCrypto.allowActions = false;
-            header?.css('color', 'red');
-            header?.css('opacity', 0.7);
-            header?.attr('title', 'No wallet was detected.');
+            if (header) header.css('color', 'red');
+            if (header) header.css('opacity', 0.7);
+            if (header) header.attr('title', 'No wallet was detected.');
         }
 
     };
 
-    if (window.ethereum) {
+    if (header && global.tinyCrypto.existEthereum()) {
         global.tinyCrypto.on('readyProvider', checkConnection);
         global.tinyCrypto.on('checkConnection', checkConnection);
         global.tinyCrypto.on('accountsChanged', checkConnection);
