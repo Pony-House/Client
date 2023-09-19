07ec392692ff49c00394a30781223dc24d07ee32
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-09-19 09:41:58 -0300

    frame wallet warn

--- src/app/organisms/settings/pages/Web3/index.jsx
+++ src/app/organisms/settings/pages/Web3/index.jsx
@@ -72,6 +72,7 @@ function Web3Section() {
 
                     {userWeb3.address ? <>
 
+                        {__ENV_APP__.electron_mode ? <p>It looks like you are using the desktop version! To use the application with your web3 wallet, you need to have the frame wallet installed on your computer.</p> : ''}
                         <p>Wallet connected: <strong className={userWeb3.valid ? 'text-success' : 'text-danger'}>{userWeb3.address}</strong></p>
 
                         <button type="button" className="btn btn-sm btn-danger my-1 my-sm-0" onClick={async () => {
@@ -85,7 +86,7 @@ function Web3Section() {
 
                     </> : <>
 
-                        <p>Connect your wallet to start configuring your account integration.</p>
+                        <p>Connect your wallet to start configuring your account integration.{__ENV_APP__.electron_mode ? ' It looks like you are using the desktop version! To use the application with your web3 wallet, you need to have the frame wallet installed on your computer.' : ''}</p>
 
                         <button type="button" className="btn btn-sm btn-primary my-1 my-sm-0" onClick={() => {
                             $.LoadingOverlay('show');
@@ -103,7 +104,7 @@ function Web3Section() {
                                 console.error(err);
                                 alert(err.message);
                             })
-                        }}><i className="fa-brands fa-ethereum" /> Connect Wallet (Metamask)</button>
+                        }}><i className="fa-brands fa-ethereum" /> Connect Wallet {window.ethereum && window.ethereum.isFrame ? '(Frame)' : '(Metamask)'}</button>
 
                     </>}
 
