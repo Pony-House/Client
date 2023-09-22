d5cc095da8bc29e100aaca068f34e1f76a013972
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-09-21 22:16:43 -0300

    __ENV_APP__.info.description

--- src/app/organisms/settings/pages/About.jsx
+++ src/app/organisms/settings/pages/About.jsx
@@ -29,7 +29,7 @@ function AboutSection() {
                                     <span className="very-small text-gray" style={{ margin: '0 var(--sp-extra-tight)' }}>{`v${cons.version}`}</span>
                                 </h4>
 
-                                <div>{__ENV_APP__.description}</div>
+                                <div>{__ENV_APP__.info.description}</div>
 
                                 <div className="mt-3">
                                     <Button className='me-1' onClick={() => window.open('https://github.com/Pony-House/Client')}>Source code</Button>
