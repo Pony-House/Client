f95cb8d45358e98f2270714f5b28dd0169cdf093
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-10-09 21:30:02 -0300

    PeopleSelector -Banner fix

--- src/app/molecules/people-selector/PeopleSelectorBanner.jsx
+++ src/app/molecules/people-selector/PeopleSelectorBanner.jsx
@@ -32,278 +32,280 @@ setInterval(() => {
 function PeopleSelectorBanner({
   name, color, user
 }) {
+  if (user) {
 
-  const mx = initMatrix.matrixClient;
+    const mx = initMatrix.matrixClient;
 
-  const statusRef = useRef(null);
-  const customStatusRef = useRef(null);
-  const profileBanner = useRef(null);
-  const userNameRef = useRef(null);
-  const displayNameRef = useRef(null);
-  const profileAvatar = useRef(null);
+    const statusRef = useRef(null);
+    const customStatusRef = useRef(null);
+    const profileBanner = useRef(null);
+    const userNameRef = useRef(null);
+    const displayNameRef = useRef(null);
+    const profileAvatar = useRef(null);
 
-  const timezoneRef = useRef(null);
-  const bioRef = useRef(null);
-  const noteRef = useRef(null);
+    const timezoneRef = useRef(null);
+    const bioRef = useRef(null);
+    const noteRef = useRef(null);
 
-  // Copy Profile Username
-  const copyUsername = {
-    tag: (event) => copyText(event, 'Username successfully copied to the clipboard.'),
-    display: (event) => copyText(event, 'Display name successfully copied to the clipboard.'),
-  };
+    // Copy Profile Username
+    const copyUsername = {
+      tag: (event) => copyText(event, 'Username successfully copied to the clipboard.'),
+      display: (event) => copyText(event, 'Display name successfully copied to the clipboard.'),
+    };
 
-  const getCustomStatus = (content) => {
+    const getCustomStatus = (content) => {
 
-    // Get Status
-    const customStatus = $(customStatusRef.current);
-    const htmlStatus = [];
-    let customStatusImg;
-    const isOffline = (content.presence === 'offline' || content.presence === 'unavailable');
+      // Get Status
+      const customStatus = $(customStatusRef.current);
+      const htmlStatus = [];
+      let customStatusImg;
+      const isOffline = (content.presence === 'offline' || content.presence === 'unavailable');
 
-    if (
-      content && content.presenceStatusMsg &&
-      (
-        (typeof content.presenceStatusMsg.msg === 'string' && content.presenceStatusMsg.msg.length > 0) ||
-        (typeof content.presenceStatusMsg.msgIcon === 'string' && content.presenceStatusMsg.msgIcon.length > 0)
-      )
-    ) {
+      if (
+        content && content.presenceStatusMsg &&
+        (
+          (typeof content.presenceStatusMsg.msg === 'string' && content.presenceStatusMsg.msg.length > 0) ||
+          (typeof content.presenceStatusMsg.msgIcon === 'string' && content.presenceStatusMsg.msgIcon.length > 0)
+        )
+      ) {
 
-      const presence = content.presenceStatusMsg;
-      const ethereumValid = (presence.ethereum && presence.ethereum.valid);
+        const presence = content.presenceStatusMsg;
+        const ethereumValid = (presence.ethereum && presence.ethereum.valid);
 
-      // Ethereum
-      if (ethereumValid) {
+        // Ethereum
+        if (ethereumValid) {
 
-        const displayName = $(displayNameRef.current);
-        let ethereumIcon = displayName.find('#dm-ethereum-icon');
-        if (ethereumIcon.length < 1) {
+          const displayName = $(displayNameRef.current);
+          let ethereumIcon = displayName.find('#dm-ethereum-icon');
+          if (ethereumIcon.length < 1) {
 
-          ethereumIcon = $('<span>', { id: 'dm-ethereum-icon', class: 'ms-2', title: presence.ethereum.address }).append(
-            $('<i>', { class: 'fa-brands fa-ethereum' })
-          );
+            ethereumIcon = $('<span>', { id: 'dm-ethereum-icon', class: 'ms-2', title: presence.ethereum.address }).append(
+              $('<i>', { class: 'fa-brands fa-ethereum' })
+            );
 
-          ethereumIcon.on('click', () => {
-            try {
-              copyToClipboard(presence.ethereum.address);
-              toast('Ethereum address successfully copied to the clipboard.');
-            } catch (err) {
-              console.error(err);
-              alert(err.message);
-            }
-          }).tooltip();
+            ethereumIcon.on('click', () => {
+              try {
+                copyToClipboard(presence.ethereum.address);
+                toast('Ethereum address successfully copied to the clipboard.');
+              } catch (err) {
+                console.error(err);
+                alert(err.message);
+              }
+            }).tooltip();
 
-          displayName.append(ethereumIcon);
+            displayName.append(ethereumIcon);
 
-        }
-
-      }
-
-      // Get Bio Data
-      if (bioRef.current) {
-
-        const bioDOM = $(bioRef.current);
-        const tinyBio = $('#dm-tiny-bio');
-
-        if (tinyBio.length > 0) {
-
-          bioDOM.removeClass('d-none')
-          if (typeof presence.bio === 'string' && presence.bio.length > 0) {
-            tinyBio.html(twemojify(presence.bio.substring(0, 190), undefined, true, false));
-          } else {
-            bioDOM.addClass('d-none')
-            tinyBio.html('');
           }
 
-        } else {
-          bioDOM.addClass('d-none')
         }
 
-      }
+        // Get Bio Data
+        if (bioRef.current) {
 
-      // Get Timezone Data
-      if (timezoneRef.current) {
+          const bioDOM = $(bioRef.current);
+          const tinyBio = $('#dm-tiny-bio');
 
-        const timezoneDOM = $(timezoneRef.current);
-        const tinyTimezone = $('#tiny-timezone');
+          if (tinyBio.length > 0) {
 
-        if (tinyTimezone.length > 0) {
-
-          timezoneDOM.removeClass('d-none');
-          if (typeof presence.timezone === 'string' && presence.timezone.length > 0) {
-
-            let timezoneText = 'null';
-            try {
-              timezoneText = moment().tz(presence.timezone).format('MMMM Do YYYY, hh:mm a');
-            } catch {
-              timezoneText = 'ERROR!';
-              timezoneDOM.addClass('d-none');
+            bioDOM.removeClass('d-none')
+            if (typeof presence.bio === 'string' && presence.bio.length > 0) {
+              tinyBio.html(twemojify(presence.bio.substring(0, 190), undefined, true, false));
+            } else {
+              bioDOM.addClass('d-none')
+              tinyBio.html('');
             }
 
-            if (timezoneAutoUpdate.html) delete timezoneAutoUpdate.html;
+          } else {
+            bioDOM.addClass('d-none')
+          }
 
-            timezoneAutoUpdate.html = tinyTimezone;
-            timezoneAutoUpdate.value = presence.timezone;
-            timezoneAutoUpdate.text = timezoneText;
+        }
 
-            tinyTimezone.text(timezoneText);
+        // Get Timezone Data
+        if (timezoneRef.current) {
+
+          const timezoneDOM = $(timezoneRef.current);
+          const tinyTimezone = $('#tiny-timezone');
+
+          if (tinyTimezone.length > 0) {
+
+            timezoneDOM.removeClass('d-none');
+            if (typeof presence.timezone === 'string' && presence.timezone.length > 0) {
+
+              let timezoneText = 'null';
+              try {
+                timezoneText = moment().tz(presence.timezone).format('MMMM Do YYYY, hh:mm a');
+              } catch {
+                timezoneText = 'ERROR!';
+                timezoneDOM.addClass('d-none');
+              }
+
+              if (timezoneAutoUpdate.html) delete timezoneAutoUpdate.html;
+
+              timezoneAutoUpdate.html = tinyTimezone;
+              timezoneAutoUpdate.value = presence.timezone;
+              timezoneAutoUpdate.text = timezoneText;
+
+              tinyTimezone.text(timezoneText);
+
+            } else {
+              timezoneDOM.addClass('d-none');
+              tinyTimezone.html('');
+            }
 
           } else {
             timezoneDOM.addClass('d-none');
-            tinyTimezone.html('');
           }
 
-        } else {
-          timezoneDOM.addClass('d-none');
+        }
+
+        // Message Icon
+        if (typeof presence.msgIcon === 'string' && presence.msgIcon.length > 0) {
+
+          customStatusImg = $('<img>', { src: presence.msgIconThumb, alt: 'icon', class: 'emoji me-1' });
+          htmlStatus.push(customStatusImg);
+
+          customStatusImg.data('pony-house-cs-normal', presence.msgIconThumb);
+          customStatusImg.data('pony-house-cs-hover', presence.msgIcon);
+
+        }
+
+        if (typeof presence.msg === 'string' && presence.msg.length > 0) {
+          htmlStatus.push($('<span>', { class: 'text-truncate cs-text' }).html(twemojify(presence.msg.substring(0, 100))));
+        }
+
+        // Get Banner Data
+        const bannerDOM = $(profileBanner.current);
+
+        if (bannerDOM.length > 0) {
+          if (typeof presence.banner === 'string' && presence.banner.length > 0) {
+            bannerDOM.css('background-image', `url("${presence.banner}")`).addClass('exist-banner');
+          } else {
+            bannerDOM.css('background-image', '').removeClass('exist-banner');
+          }
         }
 
       }
 
-      // Message Icon
-      if (typeof presence.msgIcon === 'string' && presence.msgIcon.length > 0) {
-
-        customStatusImg = $('<img>', { src: presence.msgIconThumb, alt: 'icon', class: 'emoji me-1' });
-        htmlStatus.push(customStatusImg);
-
-        customStatusImg.data('pony-house-cs-normal', presence.msgIconThumb);
-        customStatusImg.data('pony-house-cs-hover', presence.msgIcon);
-
+      // Custom Status
+      customStatus.html(htmlStatus);
+      if (!isOffline) {
+        customStatus.removeClass('d-none');
+      } else {
+        customStatus.addClass('d-none');
       }
 
-      if (typeof presence.msg === 'string' && presence.msg.length > 0) {
-        htmlStatus.push($('<span>', { class: 'text-truncate cs-text' }).html(twemojify(presence.msg.substring(0, 100))));
+      if (customStatusImg) {
+        customStatusImg.parent().parent().parent().hover(
+          () => {
+            customStatusImg.attr('src', customStatusImg.data('pony-house-cs-hover'));
+          }, () => {
+            customStatusImg.attr('src', customStatusImg.data('pony-house-cs-normal'));
+          }
+        );
       }
 
-      // Get Banner Data
-      const bannerDOM = $(profileBanner.current);
+    };
 
-      if (bannerDOM.length > 0) {
-        if (typeof presence.banner === 'string' && presence.banner.length > 0) {
-          bannerDOM.css('background-image', `url("${presence.banner}")`).addClass('exist-banner');
-        } else {
-          bannerDOM.css('background-image', '').removeClass('exist-banner');
-        }
-      }
-
-    }
-
-    // Custom Status
-    customStatus.html(htmlStatus);
-    if (!isOffline) {
-      customStatus.removeClass('d-none');
-    } else {
-      customStatus.addClass('d-none');
-    }
-
-    if (customStatusImg) {
-      customStatusImg.parent().parent().parent().hover(
-        () => {
-          customStatusImg.attr('src', customStatusImg.data('pony-house-cs-hover'));
-        }, () => {
-          customStatusImg.attr('src', customStatusImg.data('pony-house-cs-normal'));
-        }
-      );
-    }
-
-  };
-
-  if (user) {
-    getCustomStatus(getPresence(user));
-  }
-
-  useEffect(() => {
     if (user) {
-
-      // Update Status Profile
-      const updateProfileStatus = (mEvent, tinyData) => {
-
-        // Get Status
-        const status = $(statusRef.current);
-        const tinyUser = tinyData;
-
-        // Update Status Icon
-        getCustomStatus(updateUserStatusIcon(status, tinyUser));
-
-      };
-
-      // Read Events
-      const tinyNote = getDataList('user_cache', 'note', user.userId);
-
-      const tinyNoteSpacing = (event) => {
-        const element = event.target;
-        element.style.height = "5px";
-        element.style.height = `${Number(element.scrollHeight)}px`;
-      };
-
-      // Update Note
-      const tinyNoteUpdate = (event) => {
-        addToDataFolder('user_cache', 'note', user.userId, $(event.target).val(), 500);
-      };
-
-      // Read Events
-      user.on('User.currentlyActive', updateProfileStatus);
-      user.on('User.lastPresenceTs', updateProfileStatus);
-      user.on('User.presence', updateProfileStatus);
-      $(displayNameRef.current).find('> .button').on('click', copyUsername.display);
-      $(userNameRef.current).find('> .button').on('click', copyUsername.tag);
-      $(noteRef.current).on('change', tinyNoteUpdate).on('keypress keyup keydown', tinyNoteSpacing).val(tinyNote);
-      return () => {
-        $(displayNameRef.current).find('> .button').off('click', copyUsername.display);
-        $(userNameRef.current).find('> .button').off('click', copyUsername.tag);
-        $(noteRef.current).off('change', tinyNoteUpdate).off('keypress keyup keydown', tinyNoteSpacing);
-        user.removeListener('User.currentlyActive', updateProfileStatus);
-        user.removeListener('User.lastPresenceTs', updateProfileStatus);
-        user.removeListener('User.presence', updateProfileStatus);
-      };
-
+      getCustomStatus(getPresence(user));
     }
-  }, [user]);
 
-  return <>
+    useEffect(() => {
+      if (user) {
 
-    <div ref={profileBanner} className={`profile-banner profile-bg${cssColorMXID(user.userId)}`} />
+        // Update Status Profile
+        const updateProfileStatus = (mEvent, tinyData) => {
 
-    <div className='text-center profile-user-profile-avatar'>
-      <Avatar ref={profileAvatar} imageSrc={mx.mxcUrlToHttp(user.avatarUrl, 100, 100, 'crop')} imageAnimSrc={mx.mxcUrlToHttp(user.avatarUrl)} text={name} bgColor={color} size="large" isDefaultImage />
-      <i ref={statusRef} className={`user-status pe-2 ${getUserStatus(user)}`} />
-    </div>
+          // Get Status
+          const status = $(statusRef.current);
+          const tinyUser = tinyData;
 
-    <div className='card bg-bg mx-3 text-start'>
-      <div className='card-body'>
+          // Update Status Icon
+          getCustomStatus(updateUserStatusIcon(status, tinyUser));
 
-        <h6 ref={displayNameRef} className='emoji-size-fix m-0 mb-1 fw-bold display-name'><span className='button'>{twemojifyReact(name)}</span></h6>
-        <small ref={userNameRef} className='text-gray emoji-size-fix username'><span className='button'>{twemojifyReact(user.userId)}</span></small>
+        };
 
-        <div ref={customStatusRef} className='d-none mt-2 emoji-size-fix small user-custom-status' />
+        // Read Events
+        const tinyNote = getDataList('user_cache', 'note', user.userId);
 
-        <div ref={timezoneRef} className='d-none'>
+        const tinyNoteSpacing = (event) => {
+          const element = event.target;
+          element.style.height = "5px";
+          element.style.height = `${Number(element.scrollHeight)}px`;
+        };
 
-          <hr />
+        // Update Note
+        const tinyNoteUpdate = (event) => {
+          addToDataFolder('user_cache', 'note', user.userId, $(event.target).val(), 500);
+        };
 
-          <div className='text-gray text-uppercase fw-bold very-small mb-2'>Timezone</div>
-          <div id='tiny-timezone' className='emoji-size-fix small text-freedom' />
+        // Read Events
+        user.on('User.currentlyActive', updateProfileStatus);
+        user.on('User.lastPresenceTs', updateProfileStatus);
+        user.on('User.presence', updateProfileStatus);
+        $(displayNameRef.current).find('> .button').on('click', copyUsername.display);
+        $(userNameRef.current).find('> .button').on('click', copyUsername.tag);
+        $(noteRef.current).on('change', tinyNoteUpdate).on('keypress keyup keydown', tinyNoteSpacing).val(tinyNote);
+        return () => {
+          $(displayNameRef.current).find('> .button').off('click', copyUsername.display);
+          $(userNameRef.current).find('> .button').off('click', copyUsername.tag);
+          $(noteRef.current).off('change', tinyNoteUpdate).off('keypress keyup keydown', tinyNoteSpacing);
+          user.removeListener('User.currentlyActive', updateProfileStatus);
+          user.removeListener('User.lastPresenceTs', updateProfileStatus);
+          user.removeListener('User.presence', updateProfileStatus);
+        };
 
-        </div>
+      }
+    }, [user]);
 
-        <div ref={bioRef} className='d-none'>
+    return <>
 
-          <hr />
+      <div ref={profileBanner} className={`profile-banner profile-bg${cssColorMXID(user.userId)}`} />
 
-          <div className='text-gray text-uppercase fw-bold very-small mb-2'>About me</div>
-          <div id='dm-tiny-bio' className='emoji-size-fix small text-freedom' />
-
-        </div>
-
-        <hr />
-
-        <label for="tiny-note" className="form-label text-gray text-uppercase fw-bold very-small mb-2">Note</label>
-        <textarea ref={noteRef} spellCheck="false" className="form-control form-control-bg emoji-size-fix small" id="tiny-note" placeholder="Insert a note here" />
-
-
+      <div className='text-center profile-user-profile-avatar'>
+        <Avatar ref={profileAvatar} imageSrc={mx.mxcUrlToHttp(user.avatarUrl, 100, 100, 'crop')} imageAnimSrc={mx.mxcUrlToHttp(user.avatarUrl)} text={name} bgColor={color} size="large" isDefaultImage />
+        <i ref={statusRef} className={`user-status pe-2 ${getUserStatus(user)}`} />
       </div>
-    </div>
 
-  </>;
+      <div className='card bg-bg mx-3 text-start'>
+        <div className='card-body'>
 
+          <h6 ref={displayNameRef} className='emoji-size-fix m-0 mb-1 fw-bold display-name'><span className='button'>{twemojifyReact(name)}</span></h6>
+          <small ref={userNameRef} className='text-gray emoji-size-fix username'><span className='button'>{twemojifyReact(user.userId)}</span></small>
+
+          <div ref={customStatusRef} className='d-none mt-2 emoji-size-fix small user-custom-status' />
+
+          <div ref={timezoneRef} className='d-none'>
+
+            <hr />
+
+            <div className='text-gray text-uppercase fw-bold very-small mb-2'>Timezone</div>
+            <div id='tiny-timezone' className='emoji-size-fix small text-freedom' />
+
+          </div>
+
+          <div ref={bioRef} className='d-none'>
+
+            <hr />
+
+            <div className='text-gray text-uppercase fw-bold very-small mb-2'>About me</div>
+            <div id='dm-tiny-bio' className='emoji-size-fix small text-freedom' />
+
+          </div>
+
+          <hr />
+
+          <label for="tiny-note" className="form-label text-gray text-uppercase fw-bold very-small mb-2">Note</label>
+          <textarea ref={noteRef} spellCheck="false" className="form-control form-control-bg emoji-size-fix small" id="tiny-note" placeholder="Insert a note here" />
+
+
+        </div>
+      </div>
+
+    </>;
+
+  }
 }
 
 PeopleSelectorBanner.defaultProps = {
