0d3bcd93031f743b8e667a30ac6cf3ab83b7d5d2
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-12-12 01:43:45 -0300

    momentjs - time config

--- src/util/libs/momentjs.js
+++ src/util/libs/momentjs.js
@@ -1,17 +1,31 @@
 // Module
 import moment from 'moment-timezone';
+import { getAppearance } from './appearance';
 
 // Module Config
 moment.locale('en');
 
+const calendarFormat = [
+    'MM/DD/YYYY',
+    'DD/MM/YYYY',
+    'YYYY/MM/DD',
+    'YYYY/DD/MM',
+    'MM/YYYY/DD',
+    'DD/YYYY/MM',
+];
+
 const momentFormat = {
 
-    calendar: () => 'MM/DD/YYYY',
-    clock: () => 'hh:mm A',
-    clock2: () => 'hh:mm:ss A',
+    calendar: () => {
+        const tinyFormat = calendarFormat[Number(getAppearance().calendarFormat)];
+        if (typeof tinyFormat === 'string') return tinyFormat;
+        return calendarFormat[0];
+    },
+
+    clock: () => !getAppearance().is24hours ? 'hh:mm A' : 'HH:mm',
+    clock2: () => !getAppearance().is24hours ? 'hh:mm:ss A' : 'HH:mm:ss',
 
 };
-
 
 // Export Module
 export default moment;
