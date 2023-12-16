7697d6083843c2033e265d4104fd9149cc5143ce
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-12-16 06:05:06 -0300

    openOnGallery temp

--- src/util/libs/image.js
+++ src/util/libs/image.js
@@ -5,10 +5,10 @@ export function openOnGallery(imgUrl, fileName, urlRevoke = false) {
     const img = new Image();
     img.onload = function () {
         imageViewer(null, $(img), fileName, imgUrl, false, true).then((pswp) => {
-            pswp.on('close', () => {
+            /* pswp.on('close', () => {
                 pswp.destroy();
                 if (urlRevoke) window.URL.revokeObjectURL();
-            });
+            }); */
         });
     };
 
