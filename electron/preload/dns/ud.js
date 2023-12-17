2ddfe0effaf846b13def6ef753501432725c2c49
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-12-17 03:32:50 -0300

    tiny fix again

--- electron/preload/dns/ud.js
+++ electron/preload/dns/ud.js
@@ -1,20 +1,21 @@
-const resolver = (domain, resolve, udDnsIps) => {
+const resolver = (ext, domain, resolve, udDnsIps) => {
+    // console.log(ext, domain, udDnsIps);
     resolve([]);
 };
 
 const udResolver = {
-    'x': (domain, resolve, customDNS) => resolver('x', resolve, customDNS.ud),
-    'crypto': (domain, resolve, customDNS) => resolver('crypto', resolve, customDNS.ud),
-    'nft': (domain, resolve, customDNS) => resolver('nft', resolve, customDNS.ud),
-    'wallet': (domain, resolve, customDNS) => resolver('wallet', resolve, customDNS.ud),
-    'polygon': (domain, resolve, customDNS) => resolver('polygon', resolve, customDNS.ud),
-    'unstoppable': (domain, resolve, customDNS) => resolver('unstoppable', resolve, customDNS.ud),
-    'blockchain': (domain, resolve, customDNS) => resolver('blockchain', resolve, customDNS.ud),
-    'dao': (domain, resolve, customDNS) => resolver('dao', resolve, customDNS.ud),
-    '888': (domain, resolve, customDNS) => resolver('888', resolve, customDNS.ud),
-    'go': (domain, resolve, customDNS) => resolver('go', resolve, customDNS.ud),
-    'zil': (domain, resolve, customDNS) => resolver('zil', resolve, customDNS.ud),
-    'bitcoin': (domain, resolve, customDNS) => resolver('bitcoin', resolve, customDNS.ud),
+    'x': (domain, resolve, customDNS) => resolver('x', domain, resolve, customDNS.ud),
+    'crypto': (domain, resolve, customDNS) => resolver('crypto', domain, resolve, customDNS.ud),
+    'nft': (domain, resolve, customDNS) => resolver('nft', domain, resolve, customDNS.ud),
+    'wallet': (domain, resolve, customDNS) => resolver('wallet', domain, resolve, customDNS.ud),
+    'polygon': (domain, resolve, customDNS) => resolver('polygon', domain, resolve, customDNS.ud),
+    'unstoppable': (domain, resolve, customDNS) => resolver('unstoppable', domain, resolve, customDNS.ud),
+    'blockchain': (domain, resolve, customDNS) => resolver('blockchain', domain, resolve, customDNS.ud),
+    'dao': (domain, resolve, customDNS) => resolver('dao', domain, resolve, customDNS.ud),
+    '888': (domain, resolve, customDNS) => resolver('888', domain, resolve, customDNS.ud),
+    'go': (domain, resolve, customDNS) => resolver('go', domain, resolve, customDNS.ud),
+    'zil': (domain, resolve, customDNS) => resolver('zil', domain, resolve, customDNS.ud),
+    'bitcoin': (domain, resolve, customDNS) => resolver('bitcoin', domain, resolve, customDNS.ud),
 };
 
 export default udResolver;
