0d2f19d7a35368296ef0be22d78a0ead52af5969
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-01-04 00:18:02 -0300

    chatroom loader progress

--- src/app/embed/Chatroom.jsx
+++ src/app/embed/Chatroom.jsx
@@ -1,9 +1,17 @@
 /* eslint-disable camelcase */
 import React, { useState, useEffect, } from 'react';
 import PropTypes from 'prop-types';
-import { createClient } from 'matrix-js-sdk';
+import * as sdk from 'matrix-js-sdk';
+import Olm from '@matrix-org/olm';
+
+import { isAuthenticated } from '../../client/state/auth';
 
 import RoomTimeline from '../../client/state/RoomTimeline';
+import initMatrix from '../../client/initMatrix';
+import Spinner from '../atoms/spinner/Spinner';
+import ProcessWrapper from '../templates/auth/modules/ProcessWrapper';
+
+global.Olm = Olm;
 
 /*
 
@@ -17,6 +25,7 @@ import RoomTimeline from '../../client/state/RoomTimeline';
 function Chatroom({ roomId, homeserver }) {
 
     // States
+    const [isLoading, setIsLoading] = useState(1);
     const [timeline, setTimeline] = useState(null);
     const [matrixClient, setMatrixClient] = useState(null);
 
@@ -27,37 +36,68 @@ function Chatroom({ roomId, homeserver }) {
     // const timeline = new RoomTimeline(roomId);
     console.log(timeline, matrixClient);
 
+    // Load Data
     useEffect(() => {
+        if (isLoading === 1 && matrixClient === null) {
 
-        // Guest User Mode
-        const startGuest = async () => {
-            if (matrixClient === null) {
+            // Set Loading
+            setIsLoading(2);
 
-                const tmpClient = await createClient({ baseUrl: MATRIX_INSTANCE });
+            // Guest User Mode
+            const startGuest = async () => {
+
+                const tmpClient = await sdk.createClient({ baseUrl: MATRIX_INSTANCE });
                 const { user_id, device_id, access_token } = tmpClient.registerGuest();
 
-                const client = createClient({
+                const client = sdk.createClient({
                     baseUrl: MATRIX_INSTANCE,
                     accessToken: access_token,
                     userId: user_id,
                     deviceId: device_id,
-                })
+                });
 
                 client.setGuest(true);
                 return client;
 
+            };
+
+            // Start user
+            if (isAuthenticated()) {
+
+                const iId = setInterval(() => { }, 15000);
+
+                initMatrix.once('init_loading_finished', () => {
+                    clearInterval(iId);
+                    setIsLoading(0);
+                });
+
+                initMatrix.init().then(() => setMatrixClient(initMatrix.matrixClient)).catch(err => console.error(err));
+
             }
-        };
 
-        // Start Guest
-        startGuest().then(client => setMatrixClient(client)).catch(err => console.error(err));
+            // Start Guest
+            else {
+                startGuest().then(client => { setMatrixClient(client); setIsLoading(0); }).catch(err => console.error(err));
+            }
 
+        }
     }, []);
 
-    return <>
-        <div>{roomId}</div>
-        <div>{homeserver}</div>
-    </>;
+    // Loaded
+    if (!isLoading && matrixClient !== null) {
+        return <>
+            <div>{roomId}</div>
+            <div>{homeserver}</div>
+        </>;
+    }
+
+    // Loading
+    return <ProcessWrapper>
+        <Spinner />
+        <div style={{ marginTop: 'var(--sp-normal)' }}>
+            {__ENV_APP__.INFO.name} - Chatroom Embed
+        </div>
+    </ProcessWrapper>;
 
 };
 
