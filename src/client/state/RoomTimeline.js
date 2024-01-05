41bc177d8e0924c0317e4195a056da0fb8f94208
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-01-05 05:00:44 -0300

    comment

--- src/client/state/RoomTimeline.js
+++ src/client/state/RoomTimeline.js
@@ -591,6 +591,12 @@ class RoomTimeline extends EventEmitter {
   }
 
   // History Get
+  /*
+    https://matrix.org/docs/older/matrix-enact/
+    https://github.com/benparsons/matrix-enact/blob/master/src/App.js
+    https://github.com/matrix-org/matrix-js-sdk/issues/494
+    https://matrix-org.github.io/matrix-js-sdk/stable/classes/MatrixClient.html#isGuest
+  */
   /* loadScriptFromEventId(startEventId, isFirst = true) {
     const url = `${this.matrixClient.baseUrl}/_matrix/client/r0/rooms/${encodeURIComponent(this.roomId)}/context/${encodeURIComponent(startEventId)}?limit=100&access_token=${this.matrixClient.getAccessToken()}`;
     return new Promise((resolve, reject) => {
