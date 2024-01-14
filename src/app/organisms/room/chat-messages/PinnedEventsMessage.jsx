708a14f28e42186e57398b1715c12bfdf5308344
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-01-14 07:31:45 -0300

    PinnedEventsMessage - preparing filter

--- src/app/organisms/room/chat-messages/PinnedEventsMessage.jsx
+++ src/app/organisms/room/chat-messages/PinnedEventsMessage.jsx
@@ -4,13 +4,36 @@ import { objType } from '../../../../util/tools';
 
 export default function PinnedEventsMessage({ user, content, prevContent }) {
 
+    // Preparing Validator
+    const oldContent = objType(prevContent, 'object') && Array.isArray(prevContent.pinned) ? prevContent : { pinned: [] };
     const tinyCache = { added: [], removed: [] };
     if (objType(content, 'object') && Array.isArray(content.pinned)) {
 
-        console.log(content, prevContent);
+        // Check Content
+        for (const item in content.pinned) {
+            if (typeof content.pinned[item] === 'string') {
+
+                if (oldContent.pinned.indexOf(content.pinned[item]) < 0) {
+                    tinyCache.added.push(content.pinned[item]);
+                }
+
+            }
+        }
+
+        // Old Content
+        for (const item in oldContent.pinned) {
+            if (typeof oldContent.pinned[item] === 'string') {
+
+                if (content.pinned.indexOf(oldContent.pinned[item]) < 0) {
+                    tinyCache.removed.push(oldContent.pinned[item]);
+                }
+
+            }
+        }
 
     }
 
+    // Result
     console.log(tinyCache);
     return <>
         <strong>{twemojifyReact(user)}</strong>
