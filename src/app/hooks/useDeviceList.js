0fa555cb8d07af823f66c16abc09bbc7151a93bc
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-01-17 21:35:25 -0300

    matrixDevices added

--- src/app/hooks/useDeviceList.js
+++ src/app/hooks/useDeviceList.js
@@ -1,31 +1,71 @@
 import { useState, useEffect } from 'react';
+import EventEmitter from 'events';
 
 import initMatrix from '../../client/initMatrix';
 
+// Emitter
+class MatrixDevices extends EventEmitter {
+
+  constructor() {
+
+    super();
+    this.devices = [];
+
+  }
+
+  updateDevices(devices) {
+    if (Array.isArray(devices)) this.devices = devices;
+  }
+
+  getDevices() {
+    return this.devices;
+  }
+
+};
+
+const matrixDevices = new MatrixDevices();
+
+export { matrixDevices };
 export function useDeviceList() {
+
+  // Data
   const mx = initMatrix.matrixClient;
   const [deviceList, setDeviceList] = useState(null);
 
+  // Effect
   useEffect(() => {
+
     let isMounted = true;
 
+    // Start update
     const updateDevices = () => mx.getDevices().then((data) => {
       if (!isMounted) return;
-      setDeviceList(data.devices || []);
+      const devices = data.devices || [];
+      matrixDevices.updateDevices(devices);
+      matrixDevices.emit('devicesUpdated', devices);
+      setDeviceList(devices);
     });
+
+    // First check
     updateDevices();
 
+    // Get update
     const handleDevicesUpdate = (users) => {
       if (users.includes(mx.getUserId())) {
         updateDevices();
       }
     };
 
+    // Events
     mx.on('crypto.devicesUpdated', handleDevicesUpdate);
     return () => {
       mx.removeListener('crypto.devicesUpdated', handleDevicesUpdate);
       isMounted = false;
     };
+
   }, []);
+
+  // Complete
   return deviceList;
-}
+
+};
