d27fb3cca0c783af5215af9024ecd683a9a8fff9
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-02-01 22:03:58 -0300

    prettier

--- src/app/pages/App.jsx
+++ src/app/pages/App.jsx
@@ -4,10 +4,8 @@ import { isAuthenticated } from '../../client/state/auth';
 
 import Auth from '../templates/auth/Auth';
 import Client from '../templates/client/Client';
-import { refreshLang } from '../../i18';
 
 function App() {
-  refreshLang();
   return isAuthenticated() ? <Client /> : <Auth />;
 }
 
