469084271f9eb6a0528dafc617f22b6dd9103e50
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-02-02 23:43:11 -0300

    issue "global.localStorage.getItem('ponyHouse-appearance');" fixed

--- src/util/libs/appearance.js
+++ src/util/libs/appearance.js
@@ -10,54 +10,63 @@ export function getAnimatedImageUrl(url) {
 class MatrixAppearance extends EventEmitter {
   constructor() {
     super();
-    this.content = global.localStorage.getItem('ponyHouse-appearance');
+    this.Initialized = false;
+  }
 
-    try {
-      this.content = JSON.parse(this.content) ?? {};
-    } catch (err) {
-      this.content = {};
+  start() {
+    if (!this.Initialized) {
+      this.Initialized = true;
+
+      this.content = global.localStorage.getItem('ponyHouse-appearance');
+
+      try {
+        this.content = JSON.parse(this.content) ?? {};
+      } catch (err) {
+        this.content = {};
+      }
+
+      this.content.isEmbedEnabled =
+        typeof this.content.isEmbedEnabled === 'boolean' ? this.content.isEmbedEnabled : true;
+      this.content.isUNhoverEnabled =
+        typeof this.content.isUNhoverEnabled === 'boolean' ? this.content.isUNhoverEnabled : true;
+      this.content.isAnimateAvatarsEnabled =
+        typeof this.content.isAnimateAvatarsEnabled === 'boolean'
+          ? this.content.isAnimateAvatarsEnabled
+          : true;
+
+      this.content.showUserDMstatus =
+        typeof this.content.showUserDMstatus === 'boolean' ? this.content.showUserDMstatus : true;
+      this.content.pinDMmessages =
+        typeof this.content.pinDMmessages === 'boolean' ? this.content.pinDMmessages : true;
+      this.content.sendMessageEnter =
+        typeof this.content.sendMessageEnter === 'boolean' ? this.content.sendMessageEnter : true;
+
+      this.content.enableAnimParams =
+        typeof this.content.enableAnimParams === 'boolean'
+          ? this.content.enableAnimParams
+          : !!__ENV_APP__.USE_ANIM_PARAMS;
+
+      this.content.isDiscordStyleEnabled =
+        typeof this.content.isDiscordStyleEnabled === 'boolean'
+          ? this.content.isDiscordStyleEnabled
+          : !!__ENV_APP__.DISCORD_STYLE;
+
+      this.content.useFreezePlugin =
+        typeof this.content.useFreezePlugin === 'boolean' ? this.content.useFreezePlugin : false;
+
+      this.content.hidePinMessageEvents =
+        typeof this.content.hidePinMessageEvents === 'boolean'
+          ? this.content.hidePinMessageEvents
+          : false;
+      this.content.hideUnpinMessageEvents =
+        typeof this.content.hideUnpinMessageEvents === 'boolean'
+          ? this.content.hideUnpinMessageEvents
+          : false;
     }
-
-    this.content.isEmbedEnabled =
-      typeof this.content.isEmbedEnabled === 'boolean' ? this.content.isEmbedEnabled : true;
-    this.content.isUNhoverEnabled =
-      typeof this.content.isUNhoverEnabled === 'boolean' ? this.content.isUNhoverEnabled : true;
-    this.content.isAnimateAvatarsEnabled =
-      typeof this.content.isAnimateAvatarsEnabled === 'boolean'
-        ? this.content.isAnimateAvatarsEnabled
-        : true;
-
-    this.content.showUserDMstatus =
-      typeof this.content.showUserDMstatus === 'boolean' ? this.content.showUserDMstatus : true;
-    this.content.pinDMmessages =
-      typeof this.content.pinDMmessages === 'boolean' ? this.content.pinDMmessages : true;
-    this.content.sendMessageEnter =
-      typeof this.content.sendMessageEnter === 'boolean' ? this.content.sendMessageEnter : true;
-
-    this.content.enableAnimParams =
-      typeof this.content.enableAnimParams === 'boolean'
-        ? this.content.enableAnimParams
-        : !!__ENV_APP__.USE_ANIM_PARAMS;
-
-    this.content.isDiscordStyleEnabled =
-      typeof this.content.isDiscordStyleEnabled === 'boolean'
-        ? this.content.isDiscordStyleEnabled
-        : !!__ENV_APP__.DISCORD_STYLE;
-
-    this.content.useFreezePlugin =
-      typeof this.content.useFreezePlugin === 'boolean' ? this.content.useFreezePlugin : false;
-
-    this.content.hidePinMessageEvents =
-      typeof this.content.hidePinMessageEvents === 'boolean'
-        ? this.content.hidePinMessageEvents
-        : false;
-    this.content.hideUnpinMessageEvents =
-      typeof this.content.hideUnpinMessageEvents === 'boolean'
-        ? this.content.hideUnpinMessageEvents
-        : false;
   }
 
   get(folder) {
+    this.start();
     if (typeof folder === 'string' && folder.length > 0) {
       if (typeof this.content[folder] !== 'undefined') return this.content[folder];
       return null;
@@ -67,6 +76,7 @@ class MatrixAppearance extends EventEmitter {
   }
 
   set(folder, value) {
+    this.start();
     if (typeof folder === 'string') {
       this.content[folder] = value;
       global.localStorage.setItem('ponyHouse-appearance', JSON.stringify(this.content));
