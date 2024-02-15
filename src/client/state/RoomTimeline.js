a2bd9fb7097c2aac127fbbe78493f38badb31997
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-02-15 19:40:45 -0300

    rootimeline fix

--- src/client/state/RoomTimeline.js
+++ src/client/state/RoomTimeline.js
@@ -180,9 +180,9 @@ class RoomTimeline extends EventEmitter {
     this.room = !this.isGuest
       ? this.matrixClient.getRoom(roomId)
       : new Room(roomId, this.matrixClient, this.guestId, {
-          lazyLoadMembers: true,
-          timelineSupport: true,
-        });
+        lazyLoadMembers: true,
+        timelineSupport: true,
+      });
 
     // Nothing! Tiny cancel time.
     if (this.room === null) {
@@ -236,8 +236,9 @@ class RoomTimeline extends EventEmitter {
       };
 
       // First load
-      this._reset().then(() => {
-        this.loadGuestTimeline();
+      const tinyThis = this;
+      tinyThis._reset().then(() => {
+        tinyThis.loadGuestTimeline();
       });
     }
 
