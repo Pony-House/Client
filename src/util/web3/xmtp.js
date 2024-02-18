cfb09985f01eb2e051d74361bf8c17351600482e
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-02-18 06:04:38 -0300

    Xmtp start

--- src/util/web3/xmtp.js
+++ src/util/web3/xmtp.js
@@ -2,20 +2,67 @@ import { Client } from '@xmtp/xmtp-js';
 import EventEmitter from 'events';
 import { tinyCrypto } from '.';
 
+// Create a client using keys returned from getKeys
+const ENCODING = "binary";
+const mode = 'dev';
+
+export const getEnv = (env) => env === "dev" || env === "production" || env === "local" ? env : null;
+
+export const buildLocalStorageKey = (walletAddress) =>
+  walletAddress ? `xmtp:${getEnv(mode)}:keys:${walletAddress}` : "";
+
+export const loadKeys = (walletAddress) => {
+  const val = localStorage.getItem(buildLocalStorageKey(walletAddress));
+  return val ? Buffer.from(val, ENCODING) : null;
+};
+
+export const storeKeys = (walletAddress, keys) => {
+  localStorage.setItem(
+    buildLocalStorageKey(walletAddress),
+    Buffer.from(keys).toString(ENCODING),
+  );
+};
+
+export const wipeKeys = (walletAddress) => {
+  // This will clear the conversation cache + the private keys
+  localStorage.removeItem(buildLocalStorageKey(walletAddress));
+};
+
 class Xmtp extends EventEmitter {
-  constructor(mode = 'dev') {
+  constructor() {
     super();
     this.ready = false;
-    this.mode = mode;
   }
 
   async start() {
     if (__ENV_APP__.WEB3 && tinyCrypto.connected && !this.ready) {
 
       try {
+
         const signer = await tinyCrypto.getWeb3().getSigner();
-        this.xmtp = await Client.create(signer, { env: this.mode });
+        let address = await signer.getAddress();
+        if (typeof address === 'string' && mode === 'dev') address = `${address}_dev`;
+
+        let keys = loadKeys(address);
+        if (!keys) {
+          keys = await Client.getKeys(signer, {
+            // we don't need to publish the contact here since it
+            // will happen when we create the client later
+            skipContactPublishing: true,
+            // we can skip persistence on the keystore for this short-lived
+            // instance
+            persistConversations: false,
+            env: mode,
+          });
+          storeKeys(address, keys);
+        }
+
+        this.xmtp = await Client.create(null, {
+          privateKeyOverride: keys,
+        });
+
         this.ready = true;
+
       } catch (err) {
         this.ready = false;
         console.error(err);
@@ -33,8 +80,7 @@ class Xmtp extends EventEmitter {
   }
 }
 
-// const web3Talk = new Xmtp('production');
-const web3Talk = new Xmtp();
+const web3Talk = new Xmtp(mode);
 web3Talk.setMaxListeners(Infinity);
 
 if (__ENV_APP__.MODE === 'development') {
