802ac405736064a37ba5ddbcd3218d05c962f5fd
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-02-22 01:02:44 -0300

    tiny fix

--- electron/preload/db.js
+++ electron/preload/db.js
@@ -5,27 +5,30 @@ import clone from 'clone';
 const dbCache = {};
 
 contextBridge.exposeInMainWorld('tinyDB', {
-    run: (value1, value2) => new Promise((resolve, reject) => {
-        const id = generateApiKey();
-        dbCache[id] = { resolve, reject };
-        ipcRenderer.send('requestDB', 'run', id, value1, value2);
+  run: (value1, value2) =>
+    new Promise((resolve, reject) => {
+      const id = generateApiKey();
+      dbCache[id] = { resolve, reject };
+      ipcRenderer.send('requestDB', 'run', id, value1, value2);
     }),
-    all: (value1, value2) => new Promise((resolve, reject) => {
-        const id = generateApiKey();
-        dbCache[id] = { resolve, reject };
-        ipcRenderer.send('requestDB', 'all', id, value1, value2);
+  all: (value1, value2) =>
+    new Promise((resolve, reject) => {
+      const id = generateApiKey();
+      dbCache[id] = { resolve, reject };
+      ipcRenderer.send('requestDB', 'all', id, value1, value2);
     }),
-    runPing: () => new Promise((resolve, reject) => {
-        const id = generateApiKey();
-        dbCache[id] = { resolve, reject };
-        ipcRenderer.send('requestDBPing', id);
+  runPing: () =>
+    new Promise((resolve, reject) => {
+      const id = generateApiKey();
+      dbCache[id] = { resolve, reject };
+      ipcRenderer.send('requestDBPing', id);
     }),
 });
 
 ipcRenderer.on('requestDB', (event, result) => {
-    if (dbCache[result.id]) {
-        if (typeof result.err !== 'undefined') dbCache[result.id].reject(result.err);
-        else dbCache[result.id].resolve(result.result);
-        delete dbCache[result.id];
-    }
+  if (dbCache[result.id]) {
+    if (typeof result.err !== 'undefined') dbCache[result.id].reject(result.err);
+    else dbCache[result.id].resolve(clone(result.result));
+    delete dbCache[result.id];
+  }
 });
--- src/app/molecules/message/Message.jsx
+++ src/app/molecules/message/Message.jsx
@@ -320,19 +320,19 @@ const createMessageData = (
       const insertMsg = () =>
         !isJquery
           ? twemojifyReact(
-            sanitizeCustomHtml(initMatrix.matrixClient, body),
-            undefined,
-            true,
-            false,
-            true,
-          )
+              sanitizeCustomHtml(initMatrix.matrixClient, body),
+              undefined,
+              true,
+              false,
+              true,
+            )
           : twemojify(
-            sanitizeCustomHtml(initMatrix.matrixClient, body),
-            undefined,
-            true,
-            false,
-            true,
-          );
+              sanitizeCustomHtml(initMatrix.matrixClient, body),
+              undefined,
+              true,
+              false,
+              true,
+            );
 
       const msgOptions = tinyAPI.emit(
         'messageBody',
