44acc6126d3c58de99b0aa47ac7627905f97ecee
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-03-27 14:24:53 -0300

    checkUrlCache

--- src/util/libs/mediaCache.js
+++ src/util/libs/mediaCache.js
@@ -1,12 +1,21 @@
+const urlCache = {};
+const checkUrlCache = (url) => {
+  if (urlCache[url]) return urlCache[url];
+  return url;
+};
+
 // WIP FILE
 export function readImageUrl(url) {
-    return url;
+  if (!__ENV_APP__.ELECTRON_MODE) return url;
+  return checkUrlCache(url);
 }
 
 export function readVideoUrl(url) {
-    return url;
+  if (!__ENV_APP__.ELECTRON_MODE) return url;
+  return checkUrlCache(url);
 }
 
 export function readAudioUrl(url) {
-    return url;
+  if (!__ENV_APP__.ELECTRON_MODE) return url;
+  return checkUrlCache(url);
 }
