abda8dc38551ebb73a5b40c9507513d7bedbddf5
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-04-13 01:38:58 -0300

    embed key fixed

--- src/app/molecules/message/Message.jsx
+++ src/app/molecules/message/Message.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { generateApiKey } from 'generate-api-key';
 
 import { MatrixEventEvent, RoomEvent, THREAD_RELATION_TYPE } from 'matrix-js-sdk';
 
@@ -1624,7 +1623,7 @@ function Message({
                           <Embed
                             roomId={roomId}
                             threadId={threadId}
-                            key={`msg_embed_${embed.eventId}_${generateApiKey()}`}
+                            key={`msg_embed_${embed.eventId}_${embed.url}`}
                             embed={embed.data}
                           />
                         );
