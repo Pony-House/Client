2375fa184656dad077629f183c21d149488f7ad1
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-04-29 00:01:34 -0300

    blobUrlManager - tiny fix on media

--- src/app/molecules/media/Media.jsx
+++ src/app/molecules/media/Media.jsx
@@ -163,7 +163,10 @@ function Image({
     let unmounted = false;
     async function fetchUrl() {
       const myUrl = await getUrl(link, type, file, roomId, threadId);
-      if (unmounted) return;
+      if (unmounted) {
+        blobUrlManager.delete(myUrl);
+        return;
+      }
       setUrl(myUrl);
     }
     fetchUrl();
@@ -271,7 +274,10 @@ function Sticker({ name, height, width, link, file, type, roomId, threadId }) {
     let unmounted = false;
     async function fetchUrl() {
       const myUrl = await getUrl(link, type, file, roomId, threadId);
-      if (unmounted) return;
+      if (unmounted) {
+        blobUrlManager.delete(myUrl);
+        return;
+      }
       setUrl(myUrl);
     }
     fetchUrl();
@@ -390,7 +396,10 @@ function Video({
     let unmounted = false;
     async function fetchUrl() {
       const myThumbUrl = await getUrl(thumbnail, thumbnailType, thumbnailFile, roomId, threadId);
-      if (unmounted) return;
+      if (unmounted) {
+        blobUrlManager.delete(myThumbUrl);
+        return;
+      }
       setThumbUrl(myThumbUrl);
     }
 
