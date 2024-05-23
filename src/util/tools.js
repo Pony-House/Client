e55586721227f4fcd1b0816539216bb591bce254
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-05-23 00:48:04 -0300

    shuffle

--- src/util/tools.js
+++ src/util/tools.js
@@ -39,6 +39,20 @@ const blobCreator = (result) => {
   return new Blob([], { type: 'text/plain' });
 };
 
+export function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+}
+
 export function tinyAtob(b64txt) {
   const buff = Buffer.from(b64txt, 'base64');
   const txt = buff.toString('utf16le');
