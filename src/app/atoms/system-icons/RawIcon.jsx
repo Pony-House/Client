99dc09a68a031c6b46cbb4fc0c5d86a2f42c03f9
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-06-08 01:27:53 -0300

    neon color

--- src/app/atoms/system-icons/RawIcon.jsx
+++ src/app/atoms/system-icons/RawIcon.jsx
@@ -29,7 +29,27 @@ function RawIcon({
     );
   }
 
-  if (color !== null) style.color = color;
+  if (color !== null) {
+    style.color = color;
+    if (neonColor) {
+      /*
+          0 0 7px #fff,
+    0 0 10px #fff,
+    0 0 21px #fff,
+    0 0 42px #0fa,
+    0 0 82px #0fa,
+    0 0 92px #0fa,
+    0 0 102px #0fa,
+    0 0 151px #0fa;
+    */
+      style.textShadow = `0 0 1px ${color},
+      0 0 21px ${color},
+      0 0 41px ${color},
+      0 0 46px ${color},
+      0 0 51px ${color},
+      0 0 75px ${color}`;
+    }
+  }
   return (
     <i
       className={`ic-base ic-fa ic-fa-${size} ${fa}${className ? ` ${className}` : ''}`}
