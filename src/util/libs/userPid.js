f30ed73a4db3fdae17136a3ae264ea95aea81042
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-06-25 15:12:24 -0300

    UserPid Class

---
+++ src/util/libs/userPid.js
@@ -0,0 +1,53 @@
+import EventEmitter from 'events';
+import { objType } from 'for-promise/utils/lib.mjs';
+
+import initMatrix from '@src/client/initMatrix';
+import { eventMaxListeners } from '../matrixUtil';
+
+// Emitter
+class UserPid extends EventEmitter {
+  constructor() {
+    super();
+    this.vanilla = [];
+    this.firstCheck = false;
+  }
+
+  async fetch(type = null) {
+    const pidData = await initMatrix.getAccount3pid();
+
+    if (
+      objType(pidData, 'object') &&
+      Array.isArray(pidData.threepids) &&
+      pidData.threepids.length > 0
+    ) {
+      this.vanilla = pidData.threepids;
+      this.firstCheck = true;
+      for (const item in this.vanilla) {
+        this.emit(pidData.threepids[item].medium, pidData.threepids[item]);
+      }
+
+      if (typeof type !== 'string') return this.vanilla;
+      else return this.getData(type);
+    }
+    return [];
+  }
+
+  getData(type) {
+    if (this.firstCheck) return this.vanilla.find((item) => item.medium === type);
+    throw new Error('You didn\'t do the first data check using "this.fetch()"!');
+  }
+
+  get(type) {
+    if (this.firstCheck) return this.getData(type)?.address;
+    throw new Error('You didn\'t do the first data check using "this.fetch()"!');
+  }
+}
+
+// Functions and class
+const userPid = new UserPid();
+userPid.setMaxListeners(eventMaxListeners);
+export default userPid;
+
+if (__ENV_APP__.MODE === 'development') {
+  global.userPid = userPid;
+}
