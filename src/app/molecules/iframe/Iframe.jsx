f712617cc38bdfc31055527f70d4794f1f266ed0
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-06-27 17:11:17 -0300

    iframe component added.

---
+++ src/app/molecules/iframe/Iframe.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+export const postMessage = (current, msg = null) => current.contentWindow.postMessage(msg);
+
+const Iframe = React.forwardRef(
+  (
+    {
+      id = null,
+      src = null,
+      alt = null,
+      className = null,
+      allowFullScreen = null,
+      onMessage = null,
+      name = null,
+      align = null,
+      height = null,
+      sandbox = null,
+      width = null,
+      seamless = null,
+      style = null,
+    },
+    ref,
+  ) => {
+    const iframeRef = ref || useRef(null);
+    const url = new URL(src);
+
+    useEffect(() => {
+      if (iframeRef.current && onMessage) {
+        const msgFilter = (event) => {
+          if (event.origin === url.origin) {
+            onMessage(event);
+          }
+        };
+        window.addEventListener('message', msgFilter, false);
+        return () => {
+          window.removeEventListener('message', msgFilter, false);
+        };
+      }
+    });
+
+    return (
+      <iframe
+        style={style}
+        id={id}
+        src={src}
+        alt={alt}
+        ref={iframeRef}
+        className={className || 'w-100'}
+        allowFullScreen={allowFullScreen}
+        name={name}
+        align={align}
+        height={height}
+        sandbox={sandbox}
+        width={width}
+        seamless={seamless}
+      />
+    );
+  },
+);
+
+Iframe.propTypes = {
+  style: PropTypes.object,
+  allowFullScreen: PropTypes.bool,
+  name: PropTypes.string,
+  align: PropTypes.string,
+  sandbox: PropTypes.string,
+  seamless: PropTypes.bool,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  className: PropTypes.string,
+  id: PropTypes.string,
+  onMessage: PropTypes.func,
+};
+
+export default Iframe;
