72efba1ea17244889226295f9afb4045e924e1e7
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-07-21 09:40:24 -0300

    EmojiEditor - function names

--- src/util/libs/emoji/EmojiEditor.jsx
+++ src/util/libs/emoji/EmojiEditor.jsx
@@ -86,7 +86,7 @@ class EmojiEditor extends EventEmitter {
   }
 
   // Get new emoji key
-  getNewKey(pack, key) {
+  _getNewKey(pack, key) {
     if (typeof key !== 'string') return undefined;
     let newKey = key?.replace(/\s/g, '_');
     if (pack.getImages().get(newKey)) {
@@ -96,26 +96,34 @@ class EmojiEditor extends EventEmitter {
   }
 
   // Change Emoji Avatar
-  avatarChange(url, roomId, stateKey) {
+  _avatarChange(url, roomId, stateKey) {
     const { pack, sendPackContent } = !roomId
       ? this.useUserImagePack(false)
       : this.useRoomImagePack(roomId, stateKey, false);
     pack.setAvatarUrl(url);
+  }
+
+  avatarChange(url, roomId, stateKey) {
+    _avatarChange(url, roomId, stateKey);
     return sendPackContent(pack.getContent());
   }
 
   // Edit Emoji Profile
-  editProfile(name, attribution, roomId, stateKey) {
+  _editProfile(name, attribution, roomId, stateKey) {
     const { pack, sendPackContent } = !roomId
       ? this.useUserImagePack(false)
       : this.useRoomImagePack(roomId, stateKey, false);
     pack.setDisplayName(name);
     pack.setAttribution(attribution);
+  }
+
+  editProfile(name, attribution, roomId, stateKey) {
+    _editProfile(name, attribution, roomId, stateKey);
     return sendPackContent(pack.getContent());
   }
 
   // Emoji Usage Change
-  usageChange(newUsage, roomId, stateKey) {
+  _usageChange(newUsage, roomId, stateKey) {
     const { pack, sendPackContent } = !roomId
       ? this.useUserImagePack(false)
       : this.useRoomImagePack(roomId, stateKey, false);
@@ -124,34 +132,44 @@ class EmojiEditor extends EventEmitter {
     if (newUsage === 'sticker' || newUsage === 'both') usage.push('sticker');
     pack.setUsage(usage);
     pack.getImages().forEach((img) => pack.setImageUsage(img.shortcode, undefined));
+  }
 
+  usageChange(newUsage, roomId, stateKey) {
+    _usageChange(newUsage, roomId, stateKey);
     return sendPackContent(pack.getContent());
   }
 
   // Rename Emoji
-  rename(key, newKeyValue, roomId, stateKey) {
+  _rename(key, newKeyValue, roomId, stateKey) {
     const { pack, sendPackContent } = !roomId
       ? this.useUserImagePack(false)
       : this.useRoomImagePack(roomId, stateKey, false);
-    const newKey = this.getNewKey(pack, newKeyValue);
+    const newKey = this._getNewKey(pack, newKeyValue);
 
     if (!newKey || newKey === key) return;
     pack.updateImageKey(key, newKey);
+  }
 
+  rename(key, newKeyValue, roomId, stateKey) {
+    _rename(key, newKeyValue, roomId, stateKey);
     return sendPackContent(pack.getContent());
   }
 
   // Delete Emoji
-  delete(key, roomId, stateKey) {
+  _delete(key, roomId, stateKey) {
     const { pack, sendPackContent } = !roomId
       ? this.useUserImagePack(false)
       : this.useRoomImagePack(roomId, stateKey, false);
     pack.removeImage(key);
+  }
+
+  delete(key, roomId, stateKey) {
+    _delete(key, roomId, stateKey);
     return sendPackContent(pack.getContent());
   }
 
   // Usage Emoji
-  usage(key, newUsage, roomId, stateKey) {
+  _usage(key, newUsage, roomId, stateKey) {
     const { pack, sendPackContent } = !roomId
       ? this.useUserImagePack(false)
       : this.useRoomImagePack(roomId, stateKey, false);
@@ -160,23 +178,29 @@ class EmojiEditor extends EventEmitter {
     if (newUsage === 'emoticon' || newUsage === 'both') usage.push('emoticon');
     if (newUsage === 'sticker' || newUsage === 'both') usage.push('sticker');
     pack.setImageUsage(key, usage);
+  }
 
+  usage(key, newUsage, roomId, stateKey) {
+    _usage(key, newUsage, roomId, stateKey);
     return sendPackContent(pack.getContent());
   }
 
   // Add Emoji
-  add(key, url, roomId, stateKey) {
+  _add(key, url, roomId, stateKey) {
     const { pack, sendPackContent } = !roomId
       ? this.useUserImagePack(false)
       : this.useRoomImagePack(roomId, stateKey, false);
 
-    const newKey = this.getNewKey(pack, key);
+    const newKey = this._getNewKey(pack, key);
     if (!newKey || !url) return;
 
     pack.addImage(newKey, {
       url,
     });
+  }
 
+  add(key, url, roomId, stateKey) {
+    _add(key, url, roomId, stateKey);
     return sendPackContent(pack.getContent());
   }
 }
