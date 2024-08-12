6b5ddcbbb919367fbc38bfa4c3bd922eee47d083
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-08-12 14:49:00 -0300

    Service Worker - uipdater script added.

--- src/util/pwa/installer.js
+++ src/util/pwa/installer.js
@@ -71,36 +71,77 @@ export function isUsingPWA() {
 export function installPWA() {
   if ('serviceWorker' in navigator || 'ServiceWorker' in navigator) {
     // Get Items
+    const cacheChecker = { count: 0, removed: false, keep: false };
     navigator.serviceWorker
       .getRegistrations()
       .then((items) => {
         forPromise({ data: items }, async (item, fn, fnErr) => {
+          // Get Url data
+          const tinyUrl =
+            items[item].active &&
+            typeof items[item].active.scriptURL === 'string' &&
+            items[item].active.scriptURL.length > 0
+              ? new URL(items[item].active.scriptURL)
+              : {};
+
           // Remove old stuff
-          items[item]
-            .unregister()
-            .then((success) => {
-              if (!success)
-                console.error(`[PWA] Fail to remove the Service Worker ${items[item].scope}`);
-              fn();
-            })
-            .catch(fnErr);
+          if (
+            cacheChecker.count > 0 ||
+            !items[item].active ||
+            (items[item].active.state !== 'activated' &&
+              items[item].active.state !== 'activating') ||
+            tinyUrl.pathname !== '/service-worker.js'
+          ) {
+            items[item]
+              .unregister()
+              .then((success) => {
+                if (!success)
+                  console.error(`[PWA] Fail to remove the Service Worker ${items[item].scope}`);
+                else cacheChecker.removed = true;
+                fn();
+              })
+              .catch(fnErr);
+          } else if (
+            items[item].active &&
+            (items[item].active.state === 'activated' ||
+              items[item].active.state === 'activating') &&
+            tinyUrl.pathname === '/service-worker.js'
+          ) {
+            items[item]
+              .update()
+              .then((success) => {
+                if (!success)
+                  console.error(`[PWA] Fail to update the Service Worker ${items[item].scope}`);
+                else {
+                  console.log('[PWA] Service Worker Updated.');
+                  cacheChecker.keep = true;
+                  usingPWA = true;
+                }
+                fn();
+              })
+              .catch(fnErr);
+          }
+
+          cacheChecker.count++;
         })
           // Remove progress complete
           .then(() => {
             if (__ENV_APP__.MXC_SERVICE_WORKER) {
-              // Register new stuff
-              navigator.serviceWorker
-                .register('./service-worker.js', { scope: './' })
-                // Complete
-                .then(() => {
-                  console.log('[PWA] Service Worker Registered.');
-                  usingPWA = true;
-                })
-                // Error
-                .catch((err) => {
-                  console.log('[PWA] Service Worker Failed to Register.');
-                  console.error(err);
-                });
+              if (cacheChecker.removed && !cacheChecker.keep) {
+                // Register new stuff
+                navigator.serviceWorker
+                  .register('./service-worker.js', { scope: './' })
+                  // Complete
+                  .then(() => {
+                    console.log('[PWA] Service Worker Registered.');
+                    usingPWA = true;
+                  })
+                  // Error
+                  .catch((err) => {
+                    console.log('[PWA] Service Worker Failed to Register.');
+                    console.error(err);
+                  });
+              }
             }
           })
           // Error
