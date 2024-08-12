d3afb8ee821e9ff0137fd59d042d85c0a91137bd
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-08-12 02:17:58 -0300

    addEmbedItem function / env file updated.

--- src/app/molecules/message/Message.jsx
+++ src/app/molecules/message/Message.jsx
@@ -1785,67 +1785,44 @@ function Message({
           const newEmbeds = [];
           const searchEmbeds = async () => {
             let limit = 5;
+            const addEmbedItem = async (item) => {
+              if (bodyUrls[item].href && limit > 0 && !bodyUrls[item].href.startsWith('@')) {
+                const tinyEmbed = {
+                  url: bodyUrls[item],
+                  roomId,
+                  senderId,
+                  eventId,
+                };
+
+                if (
+                  bodyUrls[item].href.startsWith('http') ||
+                  bodyUrls[item].href.startsWith('https')
+                ) {
+                  try {
+                    tinyEmbed.data = await getUrlPreview(bodyUrls[item].href);
+                    tinyFixScrollChat();
+                  } catch (err) {
+                    tinyEmbed.data = null;
+                    console.error(err);
+                  }
+                } else {
+                  tinyEmbed.data = null;
+                }
+
+                newEmbeds.push(tinyEmbed);
+                limit--;
+              }
+            };
 
             const embedParallelLoad = getAppearance('embedParallelLoad');
             if (embedParallelLoad) {
               await forPromise({ data: bodyUrls }, async (item, fn) => {
-                if (bodyUrls[item].href && limit > 0 && !bodyUrls[item].href.startsWith('@')) {
-                  const tinyEmbed = {
-                    url: bodyUrls[item],
-                    roomId,
-                    senderId,
-                    eventId,
-                  };
-
-                  if (
-                    bodyUrls[item].href.startsWith('http') ||
-                    bodyUrls[item].href.startsWith('https')
-                  ) {
-                    try {
-                      tinyEmbed.data = await getUrlPreview(bodyUrls[item].href);
-                      tinyFixScrollChat();
-                    } catch (err) {
-                      tinyEmbed.data = null;
-                      console.error(err);
-                    }
-                  } else {
-                    tinyEmbed.data = null;
-                  }
-
-                  newEmbeds.push(tinyEmbed);
-                  limit--;
-                }
-
+                await addEmbedItem(item);
                 fn();
               });
             } else {
               for (const item in bodyUrls) {
-                if (bodyUrls[item].href && limit > 0 && !bodyUrls[item].href.startsWith('@')) {
-                  const tinyEmbed = {
-                    url: bodyUrls[item],
-                    roomId,
-                    senderId,
-                    eventId,
-                  };
-
-                  if (
-                    bodyUrls[item].href.startsWith('http') ||
-                    bodyUrls[item].href.startsWith('https')
-                  ) {
-                    try {
-                      tinyEmbed.data = await getUrlPreview(bodyUrls[item].href);
-                      tinyFixScrollChat();
-                    } catch (err) {
-                      tinyEmbed.data = null;
-                      console.error(err);
-                    }
-                  } else {
-                    tinyEmbed.data = null;
-                  }
-
-                  newEmbeds.push(tinyEmbed);
-                  limit--;
-                }
+                await addEmbedItem(item);
               }
             }
 
