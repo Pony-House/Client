21a659b8daae3e67b150da6bf50712ab3d58e2c3
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-08-17 13:15:40 -0300

    MutualServer - tiny fix

--- src/app/organisms/profile-viewer/tabs/MutualServers.jsx
+++ src/app/organisms/profile-viewer/tabs/MutualServers.jsx
@@ -7,7 +7,9 @@ import { selectRoom, selectRoomMode, selectTab } from '@src/client/action/naviga
 function MutualServerTitle({ userId, roomId }) {
   const { userList } = initMatrix;
   const userData = userList.getUserRooms(userId);
-  return <>{`${String(userData.rooms.length + userData.spaces.length)} Mutual Servers`}</>;
+  return (
+    <>{`${String(userData ? userData.rooms.length + userData.spaces.length : '0')} Mutual Servers`}</>
+  );
 }
 
 function MutualServerRender({ userId, requestClose }) {
@@ -15,77 +17,79 @@ function MutualServerRender({ userId, requestClose }) {
   const mx = initMatrix.matrixClient;
 
   const userData = userList.getUserRooms(userId);
-  const total = userData.rooms.length + userData.spaces.length > 0;
-  const openItem = (roomId, type) => {
-    if (type === 'space') selectTab(roomId, true);
-    else {
-      selectRoomMode('room');
-      selectRoom(roomId);
+  if (userData) {
+    const total = userData.rooms.length + userData.spaces.length > 0;
+    const openItem = (roomId, type) => {
+      if (type === 'space') selectTab(roomId, true);
+      else {
+        selectRoomMode('room');
+        selectRoom(roomId);
+      }
+      requestClose();
+    };
+
+    if (total > 0) {
+      return (
+        <>
+          {userData.spaces.length > 0 ? (
+            <>
+              <div className="small text-gray ms-2 mb-1">Spaces</div>
+              <ul class="mutual-servers m-0 p-0">
+                {userData.spaces.map((roomId) => {
+                  const room = mx.getRoom(roomId);
+
+                  return (
+                    <li class="list-group-item">
+                      <RoomSelector
+                        key={`mutual_servers_${roomId}`}
+                        name={room.name}
+                        roomId={roomId}
+                        animParentsCount={2}
+                        imageSrc={mxcUrl.getAvatarUrl(room, dfAvatarSize, dfAvatarSize)}
+                        imageAnimSrc={mxcUrl.getAvatarUrl(room)}
+                        isUnread={false}
+                        notificationCount={0}
+                        isAlert={false}
+                        onClick={() => openItem(roomId, 'space')}
+                      />
+                    </li>
+                  );
+                })}
+              </ul>
+            </>
+          ) : null}
+
+          {userData.rooms.length > 0 ? (
+            <>
+              {userData.spaces.length > 0 ? <hr className="mx-0 my-2 border-bg" /> : null}
+              <div className="small text-gray ms-2 mb-1">Rooms</div>
+              <ul class="mutual-servers m-0 p-0">
+                {userData.rooms.map((roomId) => {
+                  const room = mx.getRoom(roomId);
+
+                  return (
+                    <li class="list-group-item">
+                      <RoomSelector
+                        key={`mutual_servers_${roomId}`}
+                        name={room.name}
+                        roomId={roomId}
+                        animParentsCount={2}
+                        imageSrc={mxcUrl.getAvatarUrl(room, dfAvatarSize, dfAvatarSize)}
+                        imageAnimSrc={mxcUrl.getAvatarUrl(room)}
+                        isUnread={false}
+                        notificationCount={0}
+                        isAlert={false}
+                        onClick={() => openItem(roomId, 'room')}
+                      />
+                    </li>
+                  );
+                })}
+              </ul>
+            </>
+          ) : null}
+        </>
+      );
     }
-    requestClose();
-  };
-
-  if (total > 0) {
-    return (
-      <>
-        {userData.spaces.length > 0 ? (
-          <>
-            <div className="small text-gray ms-2 mb-1">Spaces</div>
-            <ul class="mutual-servers m-0 p-0">
-              {userData.spaces.map((roomId) => {
-                const room = mx.getRoom(roomId);
-
-                return (
-                  <li class="list-group-item">
-                    <RoomSelector
-                      key={`mutual_servers_${roomId}`}
-                      name={room.name}
-                      roomId={roomId}
-                      animParentsCount={2}
-                      imageSrc={mxcUrl.getAvatarUrl(room, dfAvatarSize, dfAvatarSize)}
-                      imageAnimSrc={mxcUrl.getAvatarUrl(room)}
-                      isUnread={false}
-                      notificationCount={0}
-                      isAlert={false}
-                      onClick={() => openItem(roomId, 'space')}
-                    />
-                  </li>
-                );
-              })}
-            </ul>
-          </>
-        ) : null}
-
-        {userData.rooms.length > 0 ? (
-          <>
-            {userData.spaces.length > 0 ? <hr className="mx-0 my-2 border-bg" /> : null}
-            <div className="small text-gray ms-2 mb-1">Rooms</div>
-            <ul class="mutual-servers m-0 p-0">
-              {userData.rooms.map((roomId) => {
-                const room = mx.getRoom(roomId);
-
-                return (
-                  <li class="list-group-item">
-                    <RoomSelector
-                      key={`mutual_servers_${roomId}`}
-                      name={room.name}
-                      roomId={roomId}
-                      animParentsCount={2}
-                      imageSrc={mxcUrl.getAvatarUrl(room, dfAvatarSize, dfAvatarSize)}
-                      imageAnimSrc={mxcUrl.getAvatarUrl(room)}
-                      isUnread={false}
-                      notificationCount={0}
-                      isAlert={false}
-                      onClick={() => openItem(roomId, 'room')}
-                    />
-                  </li>
-                );
-              })}
-            </ul>
-          </>
-        ) : null}
-      </>
-    );
   }
 
   return <center className="small">No room was found.</center>;
@@ -94,7 +98,7 @@ function MutualServerRender({ userId, requestClose }) {
 export default function MutualServersTab(menuBarItems, accountContent, existEthereum, userId) {
   const { userList } = initMatrix;
   const userData = userList.getUserRooms(userId);
-  if (userData.rooms.length + userData.spaces.length > 0)
+  if (userData && userData.rooms.length + userData.spaces.length > 0)
     menuBarItems.push({
       menu: ({ roomId, userId }) => <MutualServerTitle roomId={roomId} userId={userId} />,
       render: ({ userId, closeDialog }) => (
