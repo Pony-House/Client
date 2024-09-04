a0c983eb729332dffe91ddb82804c531344fabc6
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-09-04 05:54:46 -0300

    pwa installer --> register fixed.

--- src/util/pwa/installer.js
+++ src/util/pwa/installer.js
@@ -75,85 +75,90 @@ export function installPWA() {
     navigator.serviceWorker
       .getRegistrations()
       .then((items) => {
-        forPromise({ data: items }, async (item, fn, fnErr) => {
-          // Get Url data
-          const tinyUrl =
-            items[item].active &&
-            typeof items[item].active.scriptURL === 'string' &&
-            items[item].active.scriptURL.length > 0
-              ? new URL(items[item].active.scriptURL)
-              : {};
+        // Register new Service Worker
+        const registerNewService = () =>
+          navigator.serviceWorker
+            .register('./service-worker.js', { scope: './' })
+            // Complete
+            .then(() => {
+              console.log('[PWA] Service Worker Registered.');
+              usingPWA = true;
+            })
+            // Error
+            .catch((err) => {
+              console.log('[PWA] Service Worker Failed to Register.');
+              console.error(err);
+            });
 
-          // Remove old stuff
-          if (
-            cacheChecker.count > 0 ||
-            !items[item].active ||
-            (items[item].active.state !== 'activated' &&
-              items[item].active.state !== 'activating') ||
-            tinyUrl.pathname !== '/service-worker.js'
-          ) {
-            items[item]
-              .unregister()
-              .then((success) => {
-                if (!success)
-                  console.error(`[PWA] Fail to remove the Service Worker ${items[item].scope}`);
-                else cacheChecker.removed = true;
-                fn();
-              })
-              .catch(fnErr);
-          }
+        if (items.length > 0) {
+          forPromise({ data: items }, async (item, fn, fnErr) => {
+            // Get Url data
+            const tinyUrl =
+              items[item].active &&
+              typeof items[item].active.scriptURL === 'string' &&
+              items[item].active.scriptURL.length > 0
+                ? new URL(items[item].active.scriptURL)
+                : {};
 
-          // Update tiny stuff
-          else if (
-            __ENV_APP__.MXC_SERVICE_WORKER &&
-            items[item].active &&
-            (items[item].active.state === 'activated' ||
-              items[item].active.state === 'activating') &&
-            tinyUrl.pathname === '/service-worker.js'
-          ) {
-            items[item]
-              .update()
-              .then((success) => {
-                if (!success)
-                  console.error(`[PWA] Fail to update the Service Worker ${items[item].scope}`);
-                else {
-                  console.log('[PWA] Service Worker Updated.');
-                  cacheChecker.keep = true;
-                  usingPWA = true;
-                }
-                fn();
-              })
-              .catch(fnErr);
-          }
-
-          // Add count
-          cacheChecker.count++;
-        })
-          // Remove progress complete
-          .then(() => {
-            if (__ENV_APP__.MXC_SERVICE_WORKER) {
-              if (cacheChecker.removed && !cacheChecker.keep) {
-                // Register new stuff
-                navigator.serviceWorker
-                  .register('./service-worker.js', { scope: './' })
-                  // Complete
-                  .then(() => {
-                    console.log('[PWA] Service Worker Registered.');
-                    usingPWA = true;
-                  })
-                  // Error
-                  .catch((err) => {
-                    console.log('[PWA] Service Worker Failed to Register.');
-                    console.error(err);
-                  });
-              }
+            // Remove old stuff
+            if (
+              cacheChecker.count > 0 ||
+              !items[item].active ||
+              (items[item].active.state !== 'activated' &&
+                items[item].active.state !== 'activating') ||
+              tinyUrl.pathname !== '/service-worker.js'
+            ) {
+              items[item]
+                .unregister()
+                .then((success) => {
+                  if (!success)
+                    console.error(`[PWA] Fail to remove the Service Worker ${items[item].scope}`);
+                  else cacheChecker.removed = true;
+                  fn();
+                })
+                .catch(fnErr);
             }
+
+            // Update tiny stuff
+            else if (
+              __ENV_APP__.MXC_SERVICE_WORKER &&
+              items[item].active &&
+              (items[item].active.state === 'activated' ||
+                items[item].active.state === 'activating') &&
+              tinyUrl.pathname === '/service-worker.js'
+            ) {
+              items[item]
+                .update()
+                .then((success) => {
+                  if (!success)
+                    console.error(`[PWA] Fail to update the Service Worker ${items[item].scope}`);
+                  else {
+                    console.log('[PWA] Service Worker Updated.');
+                    cacheChecker.keep = true;
+                    usingPWA = true;
+                  }
+                  fn();
+                })
+                .catch(fnErr);
+            }
+
+            // Add count
+            cacheChecker.count++;
           })
-          // Error
-          .catch((err) => {
-            console.log('[PWA] Service Worker Failed to Unregister.');
-            console.error(err);
-          });
+            // Remove progress complete
+            .then(() => {
+              if (__ENV_APP__.MXC_SERVICE_WORKER) {
+                if (cacheChecker.removed && !cacheChecker.keep) {
+                  registerNewService();
+                }
+              }
+            })
+            // Error
+            .catch((err) => {
+              console.log('[PWA] Service Worker Failed to Unregister.');
+              console.error(err);
+            });
+        } else registerNewService();
       })
       // Error
       .catch((err) => {
