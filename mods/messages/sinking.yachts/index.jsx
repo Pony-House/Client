71474499416ba27b7aaf546220be911370b1257b
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-09-04 00:55:52 -0300

    sinkingYachts mod updated.

--- mods/messages/sinking.yachts/index.jsx
+++ mods/messages/sinking.yachts/index.jsx
@@ -3,32 +3,41 @@ import tinyAPI from '@src/util/mods';
 import { fetchFn } from '@src/client/initMatrix';
 
 export default function sinkingYachts() {
-  // Welcome
-  console.log(`[Sinking Yachts] Scammers protection mod activated! https://sinking.yachts/`);
+  if (__ENV_APP__.ELECTRON_MODE) {
+    // Welcome
+    console.log(`[Sinking Yachts] Scammers protection mod activated! https://sinking.yachts/`);
 
-  // Function
-  tinyAPI.on(
-    'openUrlChecker',
-    (data, host, protocol) =>
-      new Promise((resolve, reject) => {
-        if (
-          (protocol === 'https:' || protocol === 'http:') &&
-          (!objType(data, 'object') || !data.isScammer)
-        ) {
-          const newTinyData = { isScammer: false };
-          fetchFn(`https://phish.sinking.yachts/v2/check/${host}`, {
-            method: 'GET',
-            headers: { Accept: 'application/json' },
-          })
-            .then((res) => res.json())
-            .then((result) => {
-              newTinyData.isScammer = result;
-              resolve(newTinyData);
+    // Function
+    tinyAPI.on(
+      'openUrlChecker',
+      (data, host, protocol) =>
+        new Promise((resolve, reject) => {
+          if (
+            (protocol === 'https:' || protocol === 'http:') &&
+            (!objType(data, 'object') || !data.isScammer)
+          ) {
+            const newTinyData = { isScammer: false };
+            fetchFn(`https://phish.sinking.yachts/v2/check/${host}`, {
+              method: 'GET',
+              headers: { Accept: 'application/json' },
             })
-            .catch(reject);
-        } else {
-          resolve(data);
-        }
-      }),
-  );
+              .then((res) => res.json())
+              .then((result) => {
+                newTinyData.isScammer = result;
+                resolve(newTinyData);
+              })
+              .catch(reject);
+          } else {
+            resolve(data);
+          }
+        }),
+    );
+  }
+
+  // Invalid device
+  else {
+    console.log(
+      `[Sinking Yachts] This mod is only compativel with the desktop version. The mod was disabled automatically.`,
+    );
+  }
 }
